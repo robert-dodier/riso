79f62868b1f5663602a7b91636d98ac47d883e76
robert-dodier <robert.dodier@gmail.com>
1997-08-21 15:05:30 +0000

    changed comment blocks, changed params for update()

--- riso/riso/distributions/RegressionDensity.java
+++ riso/riso/distributions/RegressionDensity.java
@@ -1,10 +1,11 @@
 /* Copyright (c) 1997 Robert Dodier and the Joint Center for Energy Management,
  * University of Colorado at Boulder. All Rights Reserved.
  *
- * By copying this software, you agree to the following: This software is
- * distributed for non-commercial use only. (For a commercial license,
- * contact the copyright holders.) This software can be re-distributed at
- * no charge so long as this copyright statement remains intact.
+ * By copying this software, you agree to the following:
+ *  1. This software is distributed for non-commercial use only.
+ *     (For a commercial license, contact the copyright holders.)
+ *  2. This software can be re-distributed at no charge so long as
+ *     this copyright statement remains intact.
  *
  * ROBERT DODIER AND THE JOINT CENTER FOR ENERGY MANAGEMENT MAKE NO
  * REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE SOFTWARE, EITHER
@@ -21,8 +22,7 @@ import java.io.*;
 /** This class represents a conditional density based on a regression
   * function and a noise model.
   */
-
-public interface RegressionDensity
+public class RegressionDensity implements ConditionalDensity, Serializable, Cloneable
 {
 	protected int ndimensions_child, ndimensions_parent;
 
@@ -94,15 +94,20 @@ public interface RegressionDensity
 	  * @param c The parent data. Each row has a number of components equal
 	  *   to ndimensions_parent(), and the number of rows is the same
 	  *   as for <code>x</code>.
-	  * @param is_present Each component <code>is_present[i]</code> of this
-	  *   vector tells whether the corresponding datum <code>x[i]</code>
-	  *   is present or missing.
 	  * @param responsibility Each component of this vector 
 	  *   <code>responsibility[i]</code> is a scalar telling the probability
 	  *   that this density produced the corresponding datum <code>x[i]</code>.
 	  *   This is mostly intended for fitting mixture densities, although
 	  *   other uses can be imagined.
-	  * @return Whether the parameter updated succeeded or not.
+	  * @param niter_max Maximum number of iterations of the update algorithm,
+	  *   if applicable.
+	  * @param stopping_criterion A number which describes when to stop the
+	  *   update algorithm, if applicable.
+	  * @return Some indication of goodness-of-fit, such as MSE or negative
+	  *   log-likelihood.
 	  */
-	public boolean update( double[][] x, double[][] c, boolean[] is_present, double[] responsibility );
+	public double update( double[][] x, double[][] c, double[] responsibility, int niter_max, double stopping_criterion ) throws Exception
+	{
+		throw new Exception( "RegressionModel.update: not implemented." );
+	}
 }

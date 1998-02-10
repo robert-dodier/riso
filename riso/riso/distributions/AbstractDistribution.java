173718d3a9d157fd7936438e275a3fc6b2dbc281
robert-dodier <robert.dodier@gmail.com>
1998-02-10 21:30:47 +0000

    Made this class remote.

--- riso/riso/distributions/AbstractDistribution.java
+++ riso/riso/distributions/AbstractDistribution.java
@@ -1,5 +1,7 @@
 package distributions;
 import java.io.*;
+import java.rmi.*;
+import java.rmi.server.*;
 
 /** Abstract base class for unconditional distributions.
   * Since Distribution is derived from ConditionalDistribution, any
@@ -9,33 +11,38 @@ import java.io.*;
   * the definition of Distribution, but Java doesn't allow code in an
   * interface... <sigh>. So here they are.
   */
-abstract public class AbstractDistribution implements Distribution
+abstract public class AbstractDistribution extends UnicastRemoteObject implements Distribution
 {
+	/** Default constructor for this class just calls super().
+	  * It's declared here to show that it can throw a remote exception.
+	  */
+	public AbstractDistribution() throws RemoteException { super(); }
+
 	/** WHY DO I HAVE TO PUT THIS HERE???
 	  */
-	abstract public Object clone() throws CloneNotSupportedException;
+	// !!! abstract public Object remote_clone() throws CloneNotSupportedException, RemoteException;
 
 	/** The "child" in this case is just the variable itself.
 	  * So <tt>ndimensions_child</tt> equals <tt>ndimensions</tt>.
 	  */
-	public int ndimensions_child() { return ndimensions(); }
+	public int ndimensions_child() throws RemoteException { return ndimensions(); }
 
 	/** There are no parents, so return zero.
 	  */
-	public int ndimensions_parent() { return 0; }
+	public int ndimensions_parent() throws RemoteException { return 0; }
 
 	/** Context doesn't matter since this distribution is unconditional.
 	  * Return <tt>this</tt> for every value of <tt>c</tt>.
 	  */
-	public Distribution get_density( double[] c ) { return this; }
+	public Distribution get_density( double[] c ) throws RemoteException { return this; }
 
 	/** Compute the density at the point <code>x</code>.
 	  * Ignore the context <tt>c</tt>.
 	  */
-	public double p( double[] x, double[] c ) { return p(x); }
+	public double p( double[] x, double[] c ) throws RemoteException { return p(x); }
 
 	/** Return an instance of a random variable from this distribution.
 	  * Ignore the context <tt>c</tt>.
 	  */
-	public double[] random( double[] c ) { return random(); }
+	public double[] random( double[] c ) throws RemoteException { return random(); }
 }

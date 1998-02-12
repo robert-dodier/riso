3d2b96272fad4fa8050831ff67b72b211aa814f2
robert-dodier <robert.dodier@gmail.com>
1998-02-12 06:21:23 +0000

    made this class remote

--- riso/riso/test/SquashingNetTest.java
+++ riso/riso/test/SquashingNetTest.java
@@ -1,11 +1,12 @@
 package regression;
 
 import java.io.*;
+import java.rmi.*;
 import numerical.Matrix;
 
 public class SquashingNetTest extends SquashingNetwork
 {
-	public void compute_dEdw_finite_difference( double[] input, double[] target )
+	public void compute_dEdw_finite_difference( double[] input, double[] target ) throws RemoteException
 	{
 		int i;
 
@@ -34,7 +35,7 @@ public class SquashingNetTest extends SquashingNetwork
 				System.out.println( "delta["+i+"]["+j+"] == "+delta[i][j] );
 	}
 
-	public void compute_dFdx_finite_difference( double[] x )
+	public void compute_dFdx_finite_difference( double[] x ) throws RemoteException
 	{
 		int i, j;
 
@@ -70,12 +71,12 @@ public class SquashingNetTest extends SquashingNetwork
 		}
 	}
 
-	SquashingNetTest( int nin, int nhidden, int nout )
+	SquashingNetTest( int nin, int nhidden, int nout ) throws RemoteException
 	{
 		super( nin, nhidden, nout );
 	}
 
-	SquashingNetTest() { super(); }
+	SquashingNetTest() throws RemoteException { super(); }
 
 	public static void main( String args[] )
 	{
@@ -86,100 +87,108 @@ public class SquashingNetTest extends SquashingNetwork
 
 		SquashingNetTest net;
 
-		if ( "-f".equals(args[0]) )
+		try
 		{
-			net = new SquashingNetTest();
-			try { net.pretty_input( System.in ); }
-			catch (IOException e)
+			if ( "-f".equals(args[0]) )
 			{
-				System.err.println( "exception: "+e );
+				net = new SquashingNetTest();
+				try { net.pretty_input( System.in ); }
+				catch (IOException e)
+				{
+					System.err.println( "exception: "+e );
+					return;
+				}
+
+				nin = net.ndimensions_in();
+				nout = net.ndimensions_out();
+			}
+			else
+			{
+				nin = Integer.parseInt(args[0]);
+				nhid = Integer.parseInt(args[1]);
+				nout = Integer.parseInt(args[2]);
+				net = new SquashingNetTest( nin, nhid, nout );
+				for ( i = 0; i < net.nweights(); i++ )
+					net.weights_unpacked[i] = (double) i;
+			}
+
+			double[] x = new double[nin], y = new double[nout];
+			for ( i = 0; i < nin; i++ ) x[i] = 0.1 + i/10.0;
+			for ( i = 0; i < nout; i++ ) y[i] = -1 -i/10.0;
+
+			net.compute_dFdx_finite_difference( x );
+			net.compute_dEdw_finite_difference( x, y );
+
+			try { net.pretty_output( System.out, "" ); }
+			catch (IOException e) { System.out.println( "exception: "+e ); }
+
+			try
+			{
+				SquashingNetwork copy = (SquashingNetwork) ((SquashingNetwork)net).clone();
+				copy.pretty_output( System.out, "" );
+			}
+			catch (Exception e)
+			{
+				System.out.println( "problem w/ clone: "+e );
+				e.printStackTrace();
 				return;
 			}
 
-			nin = net.ndimensions_in();
-			nout = net.ndimensions_out();
-		}
-		else
-		{
-			nin = Integer.parseInt(args[0]);
-			nhid = Integer.parseInt(args[1]);
-			nout = Integer.parseInt(args[2]);
-			net = new SquashingNetTest( nin, nhid, nout );
+			double[][] xx = null, yy = null;
+			int ndata = 0;
+
+			Reader r = new BufferedReader(new InputStreamReader(System.in));
+			StreamTokenizer st = new StreamTokenizer(r);
+
+			try
+			{
+				st.nextToken();
+				ndata = (int) st.nval;
+				xx = new double[ndata][nin];
+				yy = new double[ndata][nout];
+
+				for ( i = 0; i < ndata; i++ )
+				{
+					for ( j = 0; j < nin; j++ )
+					{
+						st.nextToken();
+						xx[i][j] = st.nval;
+					}
+
+					for ( j = 0; j < nout; j++ )
+					{
+						st.nextToken();
+						yy[i][j] = st.nval;
+					}
+				}
+			}
+			catch (IOException e) { System.out.println( "exception: "+e ); }
+
+			java.util.Random random = new java.util.Random();
 			for ( i = 0; i < net.nweights(); i++ )
-				net.weights_unpacked[i] = (double) i;
-		}
+				net.weights_unpacked[i] = random.nextGaussian()/1e4;
 
-		double[] x = new double[nin], y = new double[nout];
-		for ( i = 0; i < nin; i++ ) x[i] = 0.1 + i/10.0;
-		for ( i = 0; i < nout; i++ ) y[i] = -1 -i/10.0;
+			try { net.update( xx, yy, 50, 1e-5, null ); }
+			catch (Exception e) { System.out.println( "exception: "+e ); }
 
-		net.compute_dFdx_finite_difference( x );
-		net.compute_dEdw_finite_difference( x, y );
-
-		try { net.pretty_output( System.out, " " ); }
-		catch (IOException e) { System.out.println( "exception: "+e ); }
-
-		try
-		{
-			SquashingNetwork copy = (SquashingNetwork) ((SquashingNetwork)net).clone();
-			copy.pretty_output( System.out, "-" );
-		}
-		catch (Exception e)
-		{
-			System.out.println( "problem w/ clone: "+e );
-			e.printStackTrace();
-			return;
-		}
-
-		double[][] xx = null, yy = null;
-		int ndata = 0;
-
-		Reader r = new BufferedReader(new InputStreamReader(System.in));
-		StreamTokenizer st = new StreamTokenizer(r);
-
-		try
-		{
-			st.nextToken();
-			ndata = (int) st.nval;
-			xx = new double[ndata][nin];
-			yy = new double[ndata][nout];
+			try { net.pretty_output( System.out, "" ); }
+			catch (IOException e) { System.out.println( "exception: "+e ); }
 
 			for ( i = 0; i < ndata; i++ )
 			{
-				for ( j = 0; j < nin; j++ )
-				{
-					st.nextToken();
-					xx[i][j] = st.nval;
-				}
-
-				for ( j = 0; j < nout; j++ )
-				{
-					st.nextToken();
-					yy[i][j] = st.nval;
-				}
+				System.out.print( "in: " );
+				Matrix.pretty_output( xx[i], System.out, " " );
+				System.out.print( "target: " );
+				Matrix.pretty_output( yy[i], System.out, " " );
+				System.out.print( "pred: " );
+				Matrix.pretty_output( net.F(xx[i]), System.out, " " );
+				System.out.println("");
 			}
 		}
-		catch (IOException e) { System.out.println( "exception: "+e ); }
-
-		java.util.Random random = new java.util.Random();
-		for ( i = 0; i < net.nweights(); i++ )
-			net.weights_unpacked[i] = random.nextGaussian()/1e4;
-
-		try { net.update( xx, yy, 50, 1e-5, null ); }
-		catch (Exception e) { System.out.println( "exception: "+e ); }
-
-		try { net.pretty_output( System.out, " " ); }
-		catch (IOException e) { System.out.println( "exception: "+e ); }
-
-		for ( i = 0; i < ndata; i++ )
+		catch (RemoteException e)
 		{
-			System.out.print( "in: " );
-			Matrix.pretty_output( xx[i], System.out, " " );
-			System.out.print( "target: " );
-			Matrix.pretty_output( yy[i], System.out, " " );
-			System.out.print( "pred: " );
-			Matrix.pretty_output( net.F(xx[i]), System.out, " " );
-			System.out.println("");
+			System.err.println( "remote exception: "+e );
+			System.exit(1);
 		}
 	}
 }

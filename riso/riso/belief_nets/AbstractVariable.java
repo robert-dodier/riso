487b75d7d555b3c475ae84860029e9b70d00b6eb
robert-dodier <robert.dodier@gmail.com>
1998-02-17 15:09:40 +0000

    changed return type for get_parents and get_children to Enumeration; added add_child, pretty_input, and pretty_output

--- riso/riso/belief_nets/AbstractVariable.java
+++ riso/riso/belief_nets/AbstractVariable.java
@@ -1,5 +1,8 @@
 package belief_nets;
+
+import java.io.*;
 import java.rmi.*;
+import java.util.*;
 
 public interface AbstractVariable extends Remote
 {
@@ -9,10 +12,29 @@ public interface AbstractVariable extends Remote
 
 	/** Retrieve a list of references to the parent variables of this variable.
 	  */
-	public AbstractVariable[] get_parents() throws RemoteException;
+	public Enumeration get_parents() throws RemoteException;
 
 	/** Retrieve a list of references to the child variables of this variable.
 	  */
-	public AbstractVariable[] get_children() throws RemoteException;
+	public Enumeration get_children() throws RemoteException;
 
+	/** Tell this variable to add another to its list of children.
+	  * Since the other variable may be remote, we need a method to
+	  * do this, since we can't access the children list directly.
+	  */
+	public void add_child( String child_name, AbstractVariable x ) throws RemoteException;
+
+	/** Read a description of this variable from an input stream
+	  * (as represented by a <tt>StreamTokenizer</tt>.)
+	  */
+	public void pretty_input( StreamTokenizer st ) throws IOException, RemoteException;
+
+	/** Write a description of this variable to an output stream.
+	  * This is slightly asymmetric w.r.t. to <tt>pretty_input</tt>:
+	  * this function writes the class name onto the output stream before
+	  * writing the variable name and any descriptive data; whereas
+	  * <tt>pretty_input</tt> expects that the class name has been stripped
+	  * from the input stream and the variable name is the first token.
+	  */
+	public void pretty_output( OutputStream os, String leading_ws ) throws IOException, RemoteException;
 }

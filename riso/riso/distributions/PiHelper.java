97b14f1528c6958fc61a0d438ba385df7a98f308
robert-dodier <robert.dodier@gmail.com>
1998-04-26 16:22:13 +0000

    changed names and comments to reflect correct purpose of this code >> .

--- riso/riso/distributions/PiHelper.java
+++ riso/riso/distributions/PiHelper.java
@@ -1,29 +1,31 @@
 package risotto.distributions;
+import java.util.*;
 
-public interface PiMessageHelper
+public interface PiHelper
 {
-	/** Compute a prediction message, to be sent to children. This is defined
-	  * as follows: This node is <code>y</code>, its parents are <code>x1,
-	  * x2,...,xn</code>, and the evidence on paths above this node is <code>
-	  * ``e above''<code>. The prediction message for <code>y</code> is
-	  * defined as <code>p(y|``e above'')</code>.
+	/** Compute the predictive support for a variable. This is defined
+	  * as follows: This node is <tt>y</tt>, its parents are <tt>x1,
+	  * x2,...,xn</tt>, and the evidence on paths above this node is <tt>
+	  * ``e above y''<tt>. The predictive support for <tt>y</tt> is
+	  * defined as <tt>p(y|``e above y'')</tt>.
 	  * <pre>
-	  * p(y|``e above'') = \int_x1 ... \int_xn p(y|x1,...,xn) \times
-	  *     p(x1|``e above'') ... p(xn|``e above'') dxn ... dx1
-	  *   = \int_x1 p(x1|``e above'') \int_x2 p(x2|``e above'')
-	  *     ... \int_xn p(xn|``e above'') p(y|x1,x2,...,xn) dxn ... dx2 dx1
+	  *   p(y|``e above y'') = \int_x1 ... \int_xn p(y|x1,...,xn) \times
+	  *      p(x1|``e above y'') ... p(xn|``e above y'') dxn ... dx1
+	  *    = \int_x1 p(x1|``e above y'') \int_x2 p(x2|``e above y'')
+	  *      ... \int_xn p(xn|``e above y'') p(y|x1,x2,...,xn) dxn ... dx2 dx1
 	  * </pre>
 	  * In the case of discrete variables, the integrations are summations.
 	  * An integration (or summation) can be over more than one dimension.
 	  *
 	  * @param x Node of interest.
-	  * @param pi List of incoming messages, <code>p(xk|``e above'')</code>.
-	  * @return A message <code>p(y|``e above'')</code> to be sent to child nodes.
+	  * @param pi List of incoming pi messages, <tt>p(xk|``e above y'')</tt>.
+	  *   Note that some of the evidence above <tt>y</tt> can be below <tt>xk</tt>.
+	  * @return The predictive support, <tt>p(y|``e above y'')</tt>.
 	  * @throws Exception If the arguments don't have types which can be
 	  *   processed by an implementation of this interface. If the arguments
 	  *   have types which match the name schema, then usually a message
 	  *   can be computed, although it's not guaranteed that some other
 	  *   kind of problem won't arise. 
 	  */
-	public Distribution compute_pi( ConditionalDistribution y, Distribution[] pi ) throws Exception;
+	public Distribution compute_pi( ConditionalDistribution y, Enumeration pi_messages ) throws Exception;
 }

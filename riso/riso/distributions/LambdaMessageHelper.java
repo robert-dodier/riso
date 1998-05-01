704ce15281d0b5990d2587e228f30b72eafa6644
robert-dodier <robert.dodier@gmail.com>
1998-05-01 18:06:40 +0000

    Initial revision

---
+++ riso/riso/distributions/LambdaMessageHelper.java
@@ -0,0 +1,10 @@
+package risotto.distributions;
+import java.util.*;
+
+public interface LambdaMessageHelper
+{
+	/** Compute the likelihood message from a variable to a parent.
+	  * This is defined as <tt>p(``e below''|x)</tt> ... NEEDS WORK !!!
+	  */
+	public Distribution compute_lambda_message( ConditionalDistribution px, Distribution lambda, Distribution[] pi_messages ) throws Exception;
+}

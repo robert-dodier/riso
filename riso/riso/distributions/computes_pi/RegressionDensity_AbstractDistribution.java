6b74f85141a7ae3fa4823c86b3a903db6017ce7b
robert-dodier <robert.dodier@gmail.com>
1998-07-03 13:16:52 +0000

    Initial revision

---
+++ riso/riso/distributions/computes_pi/RegressionDensity_AbstractDistribution.java
@@ -0,0 +1,26 @@
+package riso.distributions.computes_pi;
+import java.rmi.*;
+import riso.distributions.*;
+import riso.approximation.*;
+
+/** @see PiHelper
+  */
+public class RegressionDensity_AbstractDistribution implements PiHelper
+{
+	public Distribution compute_pi( ConditionalDistribution y, Distribution[] pi ) throws Exception
+	{
+		int i;
+
+		MixGaussians[] pi_approx = new MixGaussians[ pi.length ];
+
+		for ( i = 0; i < pi.length; i++ )
+		{
+			pi_approx[i] = GaussianMixApproximation.initial_mix( pi[i] );
+			double[][] pi_support = new double[1][];
+			pi_support[0] = pi[i].effective_support( 1e-8 );
+			pi_approx[i] = GaussianMixApproximation.do_approximation( pi[i], pi_approx[i], pi_support, 1e-5 );
+		}
+
+		return (new RegressionDensity_MixGaussians()).compute_pi( y, pi_approx );
+	}
+}

25f034972de69f8823109720dc0bee90cd1d075f
robert-dodier <robert.dodier@gmail.com>
1998-07-15 04:10:38 +0000

    added rebind() function to do only a rebind; took rebind out of other function defintiions/

--- riso/riso/belief_nets/AbstractBeliefNetworkContext.java
+++ riso/riso/belief_nets/AbstractBeliefNetworkContext.java
@@ -9,9 +9,12 @@ import java.rmi.*;
   */
 public interface AbstractBeliefNetworkContext extends Remote
 {
-	/** This function searches the path list to locate the belief network
+	/** This method searches the path list to locate the belief network
 	  * file. The filename must have the form "<tt>something.riso</tt>".
 	  * The name of the belief network in this case is "<tt>something</tt>".
+	  * This method does not rebind the belief network in the RMI registry;
+	  * call <tt>AbstractBeliefNetworkContext.rebind</tt> for that.
+	  *
 	  * @param bn_name Name of the belief network; does not include
 	  *   "<tt>.riso</tt>".
 	  * @return A reference to the belief network -- may be a remote reference.
@@ -26,6 +29,9 @@ public interface AbstractBeliefNetworkContext extends Remote
 	  * If the string is successfully parsed, a new belief network is 
 	  * created and a reference is placed in the reference table. The name
 	  * of the belief network is found in the description.
+	  * This method does not rebind the belief network in the RMI registry;
+	  * call <tt>AbstractBeliefNetworkContext.rebind</tt> for that.
+	  *
 	  * @param description Description of a belief network; the format is
 	  *   the same as for loading a belief network from a file.
 	  * @return A reference to the belief network -- may be a remote reference.
@@ -37,7 +43,17 @@ public interface AbstractBeliefNetworkContext extends Remote
 	/** Given the name of a belief network, this method returns a reference
 	  * to that belief network. If the belief network is not already loaded,
 	  * it is loaded from the local filesystem.
+	  * This method does not rebind the belief network in the RMI registry;
+	  * call <tt>AbstractBeliefNetworkContext.rebind</tt> for that.
 	  */
 	public AbstractBeliefNetwork get_reference( String bn_name ) throws RemoteException;
+
+	/** Rebinds the given reference in the RMI registry.
+	  * The URL is based on the full name of the argument <tt>bn</tt>,
+	  * which has the form <tt>host.locale.domain/server-name</tt>, or
+	  * <tt>host.local.domain:port/server-name</tt> if the RMI registry
+	  * port is different from the default.
+	  */
+	public void rebind( AbstractBeliefNetwork bn ) throws RemoteException;
 }
 

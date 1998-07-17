c590a36edfc2246fa4f6c96797ab060f894f8fc5
robert-dodier <robert.dodier@gmail.com>
1998-07-17 04:28:17 +0000

    massive revs; not there yet, though..

--- riso/riso/distributions/computes_lambda_message/AbstractConditionalDistribution_AbstractDistribution_AbstractDistribution.java
+++ riso/riso/distributions/computes_lambda_message/AbstractConditionalDistribution_AbstractDistribution_AbstractDistribution.java
@@ -9,25 +9,41 @@ import numerical.*;
 /** This class implements a lambda message helper for a variable <tt>x</tt> with
   * one or more parents <tt>u1,...,un</tt>. Except for the parent to which
   * we are sending the lambda message, each parent sends a pi message to
-  * <tt>x</tt>.
+  * <tt>x</tt>. Let us suppose that we are sending the lambda message to
+  * parent <tt>uk</tt>. The lambda message is defined as
   * <pre>
-  *   p( e below x + e above x \ e_x(u) | u )
-  *     = \int p( x, e below x + e above x \ e_x(u) | u ) dx
-  *     = \int p( x | e below x + e above x \ e_x(u), u )
-  *         p( 
-  *     = \int p( e below x | x ) p( x | u ) dx
-  *     = \int lambda(x) p(x|u) dx
+  *   p( e \ e_u1(above) | uk )
+  *     = \int p( x, e \ e_uk(above) | uk ) dx
+  *     = \int p( x, e_x(below) + e_x(above) \ e_uk(above) | uk ) dx
+  *     = \int p( e_x(below) | x ) p( x | e_x(above) \ e_uk(above), uk ) p( e_x(above) \ e_uk(above) | uk ) dx
+  *     = p( e_x(above) \ e_uk(above) ) \int p( e_x(below) | x ) \int ... \int p( x | u1,...,un ) p( u1,...,un \ uk |  e_x(above) \ e_uk(above), uk )  du1 ... du_{k-1} du_{k+1} ... du_n dx
   * </pre>
-  * Note that this integral is a function of the parent <tt>u</tt>.
-  * The message which is sent up the parent is a best-fitting approximation
-  * (a mixture of Gaussians). In order to find the best approximation, 
-  * two helpers are needed: one to compute values of the integral (this is
-  * the target for the approximation), and the other to compute the integrand
-  * (which is then used to compute the integral).
+  * The factor <tt>p( e_x(above) \ e_uk(above) )</tt> doesn't depend on <tt>uk</tt> so we can ignore it
+  * (a lambda message need not integrate to anything in particular).
+  * Now note that 
+  * <pre>
+  *    p( u1,...,un \ uk | e_x(above) \ e_uk(above), uk ) = \prod_{j \neq k} p( uj | e_uj(above) )
+  * </pre>
+  * so finally we have
+  * <pre>
+  *   p( e \ e_u1(above) | uk ) = \int p( e_x(below) | x ) \int ... \int p( x | u1,...,un ) \prod_{j \neq k} p( uj | e_uj(above) ) du1 ... du_{k-1} du_{k+1} ... du_n dx
+  * </pre>
+  * In this last equation the lambda function of <tt>x</tt> appears, <tt>p( e_x(below) | x )</tt>,
+  * and the pi messages coming to <tt>x</tt> from parents other than <tt>uk</tt>,
+  * <tt>p( uj | e_uj(above) ), j \neq k</tt>. The conditional distribution of <tt>x</tt> given
+  * all its parents, <tt>p( x | u1,...,un )</tt>, links the other pieces together.
+  *
+  * <p> Note that this integral is a function of the parent <tt>uk</tt>.
+  * The message which is sent up the parent is NOT a Gaussian mixture or other approximation;
+  * the message is a direct representation of the integral. The evaluation of the integral is
+  * put off until the posterior or lambda of <tt>uk</tt> needs to be computed.
+  *
+  * <p> To construct the lambda message, two helpers are needed: one to compute values of the integral,
+  * and the other to compute the integrand (which is then used to compute the integral).
   */
-public class RegressionDensity_AbstractDistribution_ implements LambdaMessageHelper
+public class AbstractConditionalDistribution_AbstractDistribution_AbstractDistribution implements LambdaMessageHelper
 {
-	public static class Integral extends AbstractDistribution implements Callback_1d
+	public static class Integral_wrt_x extends AbstractDistribution implements Callback_1d
 	{
 		public class Integrand implements Callback_1d
 		{
@@ -41,6 +57,26 @@ public class RegressionDensity_AbstractDistribution_ implements LambdaMessageHel
 				return lambda.p( x_array ) * pxu.p( x_array, u_array );
 			}
 		}
+
+		public class Integral_wrt_u extends AbstractDistribution implements Callback_nd
+		{
+			public class Integrand implements Callback_nd
+			{
+				public double f( double[] u_in )
+				{
+					int i;
+					double pi_product = 1;
+
+					for ( i = 0; i < u_array.length; i++ )
+					{
+						if ( i == skip_index ) continue;
+
+						u1[0] = u_array[i];
+						pi_product *= pi_messages[i].p( u1 );
+					}
+
+					return conditional.p( x1, u_array ) * pi_product;
+				}
 
 		ConditionalDistribution pxu;
 		Distribution lambda;

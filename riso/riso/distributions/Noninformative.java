d2b237a8fb8ac1baef14fb79f00d1f40680a27b1
robert-dodier <robert.dodier@gmail.com>
1998-07-19 03:55:28 +0000

    added effective_support, which always throws SupportNotWellDefinedException.

--- riso/riso/distributions/Noninformative.java
+++ riso/riso/distributions/Noninformative.java
@@ -10,16 +10,24 @@ public class Noninformative extends AbstractDistribution
 	public Noninformative() throws RemoteException {}
 
 	/** Since all <tt>Noninformative</tt> lambda messages are alike,
-	  * this method simply returns a new <tt>Noninformative</tt>.
+	  * this method simply returns <tt>this</tt>.
 	  * This method doesn't make much sense, but it's harmless.
 	  */
 	public Object remote_clone() throws CloneNotSupportedException, RemoteException
 	{
-		return new Noninformative();
+		return this;
 	}
 
 	/** Computes the density at the point <code>x</code>. 
 	  * Always returns 1.
 	  */
 	public double p( double[] x ) throws RemoteException { return 1; }
+
+	/** The support for a noninformative lambda or lambda message is unbounded.
+	  * @throws SupportNotWellDefinedException Always thrown.
+	  */
+	public double[] effective_support( double epsilon ) throws RemoteException
+	{
+		throw new SupportNotWellDefinedException( "Noninformative: support is not well-defined." );
+	}
 }

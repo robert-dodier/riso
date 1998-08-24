de91cd68d52c8cfb330022bc2561771ff259af75
robert-dodier <robert.dodier@gmail.com>
1998-08-24 16:41:08 +0000

    Contains the ignored_scale method; put this on branch -- dropping it from main development path.

--- riso/riso/distributions/TrivialNoninformativeLambdaHelper.java
+++ riso/riso/distributions/TrivialNoninformativeLambdaHelper.java
@@ -8,4 +8,10 @@ public class TrivialNoninformativeLambdaHelper implements LambdaHelper
 	{
 		return new Noninformative();
 	}
+
+	public double ignored_scale( Distribution[] lambda_messages )
+	{
+		// compute_lambda returns exact result.
+		return 1;
+	}
 }
--- riso/riso/distributions/TrivialSoleLambdaHelper.java
+++ riso/riso/distributions/TrivialSoleLambdaHelper.java
@@ -9,4 +9,10 @@ public class TrivialSoleLambdaHelper implements LambdaHelper
 	{
 		return (Distribution) lambda_messages[0].remote_clone();
 	}
+
+	public double ignored_scale( Distribution[] lambda_messages )
+	{
+		// compute_lambda returns exact result.
+		return 1;
+	}
 }

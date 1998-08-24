149fc70d6a60c88304afd0f5d71de59567f570e7
robert-dodier <robert.dodier@gmail.com>
1998-08-24 22:46:33 +0000

    added output of 100 points in the effective suppport of the result

--- riso/riso/test/MixProductTest.java
+++ riso/riso/test/MixProductTest.java
@@ -23,6 +23,18 @@ public class MixProductTest
 			MixGaussians product = MixGaussians.product_mixture( mixtures );
 
 			System.err.println( "product:\n"+product.format_string("") );
+
+			double[] support = product.effective_support( 1e-6 );
+			double[] x = new double[1];
+			double x0 = support[0], x1 = support[1], dx = (x1-x0)/100;
+
+			for ( x[0] = x0+dx/2; x[0] < x1; x[0] += dx )
+			{
+				double prodp = 1;
+				for ( int j = 0; j < mixtures.length; j++ )
+					prodp *= mixtures[j].p( x );
+				System.err.println( "x: "+x[0]+" prodp: "+prodp+" product.p: "+product.p(x) );
+			}
 		}
 		catch (Exception e)
 		{

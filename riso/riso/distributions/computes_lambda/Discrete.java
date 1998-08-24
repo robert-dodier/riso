c940819ad7abcfe195eea0e00a32f0d7a9014ebf
robert-dodier <robert.dodier@gmail.com>
1998-08-24 17:05:07 +0000

    added code to normalize result

--- riso/riso/distributions/computes_lambda/Discrete.java
+++ riso/riso/distributions/computes_lambda/Discrete.java
@@ -9,12 +9,14 @@ public class Discrete implements LambdaHelper
 	  */
 	public riso.distributions.Distribution compute_lambda( riso.distributions.Distribution[] lambda_messages ) throws Exception
 	{
+		int i, j;
+
 		// Some of the lambda messages may be null or noninformative; skip over those.
 
 		riso.distributions.Discrete p = null;
 		boolean first_informative = true;
 
-		for ( int i = 0; i < lambda_messages.length; i++ )
+		for ( i = 0; i < lambda_messages.length; i++ )
 		{
 			if ( lambda_messages[i] == null || lambda_messages[i] instanceof Noninformative )
 				continue;
@@ -30,10 +32,17 @@ public class Discrete implements LambdaHelper
 			if ( q.probabilities.length != p.probabilities.length )
 				throw new IllegalArgumentException( "computes_lambda.Discrete.compute_lambda: some lambda messages have different lengths." );
 
-			for ( int j = 0; j < p.probabilities.length; j++ )
+			for ( j = 0; j < p.probabilities.length; j++ )
 				p.probabilities[j] *= q.probabilities[j];
 		}
 
+		// Strictly speaking, we don't have to normalize the result,
+		// but this code may be called from someplace that needs it normalized.
+
+		double sum = 0;
+		for ( i = 0; i < p.probabilities.length; i++ ) sum += p.probabilities[i];
+		for ( i = 0; i < p.probabilities.length; i++ ) p.probabilities[i] /= sum;
+		
 		return p;
 	}
 }

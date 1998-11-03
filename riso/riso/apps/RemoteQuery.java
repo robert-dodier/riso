0bf3e6b9f0fc15e836709195df7635fb69b0d6e7
robert-dodier <robert.dodier@gmail.com>
1998-11-03 18:47:47 +0000

    this works fine, but only with one bn per run

--- riso/riso/apps/RemoteQuery.java
+++ riso/riso/apps/RemoteQuery.java
@@ -33,35 +33,59 @@ public class RemoteQuery
 			System.err.println( "RemoteQuery: url: "+url );
 			AbstractBeliefNetwork bn = (AbstractBeliefNetwork) Naming.lookup( url );
 
+			AbstractVariable[] bnv = bn.get_variables();
+			System.out.println( "RemoteQuery: variables in "+bn.get_fullname()+":" );
+			for ( i = 0; i < bnv.length; i++ )
+			{
+				System.out.println( "\t"+bnv[i].get_name() );
+			}
+
 			SmarterTokenizer st = new SmarterTokenizer( new BufferedReader( new InputStreamReader( System.in ) ) );
 
 			for ( st.nextToken(); st.ttype != StreamTokenizer.TT_EOF; st.nextToken() )
 			{
-				String x_name = st.sval;
-				st.nextToken();
-				if ( "=".equals( st.sval ) )
+				try
 				{
-					st.nextToken();
-					double e = Format.atof( st.sval );
-					System.out.println( "RemoteQuery: set "+x_name+" to "+e );
-					bn.assign_evidence( bn.name_lookup( x_name ), e );
+					if ( "?".equals( st.sval ) )
+					{
+						System.out.println( "RemoteQuery: belief network: " );
+						System.out.print( bn.format_string() );
+					}
+					else
+					{
+						String x_name = st.sval;
+						st.nextToken();
+						if ( st.ttype == '=' )
+						{
+							st.nextToken();
+							double e = Format.atof( st.sval );
+							System.out.println( "RemoteQuery: set "+x_name+" to "+e );
+							bn.assign_evidence( bn.name_lookup( x_name ), e );
+						}
+						else if ( "?".equals( st.sval ) )
+						{
+							Distribution xposterior = bn.get_posterior( bn.name_lookup( x_name ) );
+							System.out.println( "RemoteQuery: posterior for "+x_name+":" );
+							System.out.print( "  "+xposterior.format_string( "\t" ) );
+						}
+						else if ( "-".equals( st.sval ) )
+						{
+							bn.clear_posterior( bn.name_lookup( x_name ) );
+							System.out.println( "RemoteQuery: clear evidence: "+x_name );
+						}
+					}
 				}
-				else if ( "?".equals( st.sval ) )
+				catch (RemoteException e)
 				{
-					Distribution xposterior = bn.get_posterior( bn.name_lookup( x_name ) );
-					System.out.println( "RemoteQuery: posterior for "+x_name+":" );
-					System.out.print( "  "+xposterior.format_string( "\t" ) );
-				}
-				else if ( "-".equals( st.sval ) )
-				{
-					bn.clear_evidence( bn.name_lookup( x_name ) );
-					System.out.println( "RemoteQuery: clear evidence: "+x_name );
+					e.printStackTrace();
+					System.err.println( "RemoteQuery: operation failed; stagger forward. " );
 				}
 			}
 		}
 		catch (Exception ex)
 		{
 			ex.printStackTrace();
+			System.exit(1);
 		}
 
 		System.exit(0);

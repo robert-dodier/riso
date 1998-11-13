97a21738f2b2877421550ea1f7d7269286f3426d
robert-dodier <robert.dodier@gmail.com>
1998-11-13 19:41:51 +0000

    added a bunch of stuff... now puts stuff in the global directory, if the global directory is running

--- riso/riso/apps/PublishNetwork.java
+++ riso/riso/apps/PublishNetwork.java
@@ -3,13 +3,17 @@ package riso.apps;
 import java.rmi.*;
 import java.util.*;
 import riso.belief_nets.*;
+import GlobalDirectory;
 
 public class PublishNetwork
 {
+	public static Vector souvenirs = new Vector();
+
 	public static void main( String[] args )
 	{
 		String context_name = "";
 		Vector bn_names = new Vector();
+		String directory_name = "sonero/global-directory";
 
 		for ( int i = 0; i < args.length; i++ )
 		{
@@ -23,6 +27,9 @@ public class PublishNetwork
 			case 'c':
 				context_name = args[++i];
 				break;
+			case 'd':
+				directory_name = args[++i];
+				break;
 			}
 		}
 
@@ -31,6 +38,12 @@ public class PublishNetwork
 
 		try
 		{
+			String directory_url = "rmi://"+directory_name;
+			GlobalDirectory directory = null;
+			
+			try { directory = (GlobalDirectory) Naming.lookup( directory_url ); }
+			catch (Exception e) { System.err.println( "PublishNetwork: attempt to contact "+directory_url+" failed." ); }
+
 			if ( "".equals(context_name) )
 			{
 				BeliefNetworkContext local_bnc = new BeliefNetworkContext();
@@ -38,9 +51,9 @@ public class PublishNetwork
 			}
 			else
 			{
-				String url = "rmi://"+context_name;
-				System.err.println( "PublishNetwork: url: "+url );
-				bnc = (AbstractBeliefNetworkContext) Naming.lookup( url );
+				String context_url = "rmi://"+context_name;
+				System.err.println( "PublishNetwork: context_url: "+context_url );
+				bnc = (AbstractBeliefNetworkContext) Naming.lookup( context_url );
 			}
 
 			for ( int i = 0; i < bn_names.size(); i++ )
@@ -49,15 +62,19 @@ public class PublishNetwork
 				bn = (AbstractBeliefNetwork) bnc.load_network( bn_name );
 				System.err.println( "PublishNetwork: rebind belief net: "+bn.get_fullname() );
 				bnc.rebind( bn );
+
+				if ( directory != null )
+				{
+					try { souvenirs.addElement( directory.make_entry( bn.get_fullname() ) ); }
+					catch (Exception e) { System.err.println( "PublishNetwork: attempt to make directory entry "+bn.get_fullname()+" failed." ); }
+				}
 			}
 		}
 		catch (Exception e)
 		{
-			System.err.println( "PublishNetwork: attempt failed: "+e );
+			System.err.println( "PublishNetwork: attempt failed: " );
 			e.printStackTrace();
 			System.exit(1);
 		}
-
-		// System.exit(0);
 	}
 }

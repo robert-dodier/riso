1871da48f9ef079824d089d0c2b4568edb13b80c
robert-dodier <robert.dodier@gmail.com>
1998-11-19 16:36:12 +0000

    Initial revision

---
+++ riso/riso/test/TextObserver.java
@@ -0,0 +1,87 @@
+package riso.render;
+
+import java.rmi.*;
+import java.util.*;
+import riso.belief_nets.*;
+import riso.distributions.*;
+import riso.remote_data.*;
+
+public class TextObserver extends RemoteObserverImpl
+{
+	public TextObserver() throws RemoteException {}
+
+	/** This method is called by the variable being watched after 
+	  * the variable has changed. When that happens, print out a report
+	  * about the current posterior distribution of the variable.
+	  */
+	public void update( RemoteObservable o, Object of_interest, Object arg ) throws RemoteException
+	{
+		try
+		{
+			AbstractVariable x = (AbstractVariable) of_interest;
+			Distribution p = (Distribution) arg;
+			System.out.println( "local time: "+(new Date())+" "+x.get_fullname()+" mean: "+p.expected_value()+", stddev: "+p.sqrt_variance() );
+		}
+		catch (Exception e)
+		{
+			System.err.println( "TextObserver.update: barf: "+e );
+		}
+	}
+
+	/** Register this object as an observer interested in the
+	  * variables listed on the command line by <tt>-v</tt> options.
+	  */
+	public static void main( String args[] )
+	{
+		try
+		{
+			BeliefNetworkContext bnc = new BeliefNetworkContext();
+			Remote bn = null;
+			String bn_name = null;
+
+			TextObserver to = new TextObserver();
+
+			for ( int i = 0; i < args.length; i++ )
+			{
+				if ( args[i].charAt(0) != '-' ) continue;
+
+				switch ( args[i].charAt(1) )
+				{
+				case 'b':
+					bn_name = args[++i];
+					try { bn = bnc.get_reference( bn_name ); }
+					catch (Exception e)
+					{
+						System.err.println( "TextObserver: can't get reference to "+bn_name+"; give up." );
+						e.printStackTrace();
+						System.exit(1);
+					}
+					break;
+				case 'v':
+					String xname = args[++i];
+					if ( bn_name == null )
+						System.err.println( "TextObserver: can't get reference to "+xname+"; don't know belief network yet." );
+					else
+					{
+						try
+						{
+							AbstractVariable x = ((AbstractBeliefNetwork)bn).name_lookup( xname );
+							((RemoteObservable)bn).add_observer( to, x );
+						}
+						catch (Exception e)
+						{
+							System.err.println( "TextObserver: attempt to process "+xname+" failed." );
+							e.printStackTrace();
+						}
+					}
+					break;
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
---
+++ riso/riso/unfinished/DistributionStripChart.java
@@ -0,0 +1,77 @@
+/** <applet code="DistributionStripChart" width=400 height=200> </applet>
+  */
+import java.applet.*;
+import java.awt.*;
+
+public class VariableStripChart extends Applet
+{
+	AbstractVariable x;
+	int[] x, y;
+	Rect window, viewport;
+
+	public VariableStripChart( String x_name )
+	{
+		x = (new BeliefNetworkContext()).get_reference( x_name );
+
+		if ( ! x.is_discrete() )
+			throw new IllegalArgumentException( "DistributionStripChart: p is "+p.getClass()+"; must be discrete." );
+
+        Dimension s = size();
+
+		this.p = p;
+		double[] support = p.effective_support(1e-3);
+
+		int N = 1 + (int) support[1];
+		x = new int[N];
+		y = new int[N];
+		double[] xi = new double[1];
+		double yi;
+
+		for ( int i = 0; i <= N; i++ )
+		{
+			xi[0] = i;
+			yi = p.p( xi );
+			x[i] = translate_x( xi[0] );
+			y[i] = translate_y( yi );
+		}
+	}
+
+    public void paint(Graphics g)
+	{
+        Dimension d = size();
+
+        g.setColor(Color.black);
+
+		if ( p.is_discrete() )
+		{
+			for ( int i = 0; i < x.length; i++ )
+			{
+				g.drawLine( x[i], 0, x[i], y[i] );
+				g.fillOval( x[i], y[i], 3, 3 );
+			}
+		}
+		else
+		{
+			g.drawPolyline( x, y, x.length );
+		}
+
+        g.setColor(Color.lightGray);
+        g.drawRect( viewport );
+		g.drawLine( viewport.x0, 0, viewport.x1, 0 );
+    }
+
+    public boolean mouseUp(java.awt.Event evt, int x, int y)
+	{
+        // RESET SCALE AND TRANSLATION ???
+        repaint();
+        return true;
+    }
+
+    public boolean mouseDrag( java.awt.Event evt, int x, int y )
+    {
+        // SET RUBBER BOX ???
+        repaint();
+        return true;
+    }
+}
+

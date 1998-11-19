b716433feba539e5ee957bb99e1145721b70fc87
robert-dodier <robert.dodier@gmail.com>
1998-11-19 16:33:41 +0000

    serious hacking -- works pretty well though.

--- riso/riso/render/PlotDistribution.java
+++ riso/riso/render/PlotDistribution.java
@@ -5,6 +5,7 @@
   */
 import java.applet.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.*;
@@ -15,7 +16,9 @@ import riso.remote_data.*;
 public class PlotDistribution extends Applet implements RemoteObserver
 {
 	AbstractVariable variable;
+	Distribution p;
 	int[] x, y;
+	double xmin, xmax, xmean;
 	double win_x0, win_y0, win_width, win_height;
 	int vpt_x0, vpt_y0, vpt_width, vpt_height;
 	Dimension size;
@@ -58,8 +61,11 @@ System.err.println( "bn_name: "+bn_name+", variable_name: "+variable_name );
 			Remote bn = bnc.get_reference( bn_name );
 			variable = ((AbstractBeliefNetwork)bn).name_lookup( variable_name );
 			((RemoteObservable)bn).add_observer( this, variable );
+			p = variable.get_posterior();
+			set_geometry();
+			this.addComponentListener( new PlotComponentListener() );
 		}
-		catch (RemoteException e)
+		catch (Exception e)
 		{
 			e.printStackTrace();
 			throw new RuntimeException( "PlotDistribution.init: "+e );
@@ -75,96 +81,10 @@ System.err.println( "bn_name: "+bn_name+", variable_name: "+variable_name );
 		try
 		{
 			AbstractVariable xx = (AbstractVariable) of_interest;
-			Distribution p = (Distribution) arg;
+			p = (Distribution) arg;
 System.out.println( "update: local time: "+(new Date())+" "+xx.get_fullname() );
-System.out.println( "\t"+" mean: "+p.expected_value()+", stddev: "+p.sqrt_variance() );
-
-			double[] support = p.effective_support(1e-3);
-
-			size = getSize();
-			vpt_x0 = 25;
-			vpt_y0 = 25;
-			vpt_width = size.width-2*25;
-			vpt_height = size.height-2*25;
-
-			win_x0 = support[0];
-			win_y0 = 0;
-			win_width = support[1]-support[0];
-			// Set win_height based on maximum y value, below.
-
-			double y_max = -1;
-
-			// Store the points to be plotted in window coordinate;
-			// translate to viewport coords after fudging window parameters.
-			double[] win_x, win_y;
-
-			if ( variable.is_discrete() )
-			{
-System.err.println( "\t"+variable.get_fullname()+" is discrete." );
-				y_max = 1; // set maximum for discrete probabilities
-				int N = 1 + (int) support[1];
-				win_x = new double[N];
-				win_y = new double[N];
-				double[] xi = new double[1];
-
-				for ( int i = 0; i < N; i++ )
-				{
-					xi[0] = i;
-					win_x[i] = xi[0];
-					win_y[i] = p.p( xi );
-				}
-			}
-			else
-			{
-System.err.println( "\t"+variable.get_fullname()+" is NOT discrete." );
-				int N = 100;
-				double x0 = support[0], x1 = support[1];
-				double dx = (x1-x0)/N;
-
-				win_x = new double[N];
-				win_y = new double[N];
-				double[] xi = new double[1];
-				double yi;
-
-				for ( int i = 0; i < N; i++ )
-				{
-					xi[0] = x0 + (i+0.5)*dx;
-					yi = p.p( xi );
-					if ( yi > y_max ) y_max = yi;
-					win_x[i] = xi[0];
-					win_y[i] = yi;
-				}
-			}
-
-			win_height = y_max;
-
-			// Now fudge the window parameters so that plotted stuff
-			// doesn't touch the edges of the viewport.
-
-			double win_xmargin = win_width*0.1, win_ymargin = win_height*0.1;
-			win_x0 -= win_xmargin;
-			win_y0 -= win_ymargin;
-			win_width += 2*win_xmargin;
-			win_height += 2*win_ymargin;
-
-			// Now translate window coords to viewport coords.
-
-			x = new int[ win_x.length ];
-			y = new int[ win_y.length ];
-
-			for ( int i = 0; i < win_x.length; i++ )
-			{
-				x[i] = translate_x( win_x[i] );
-				y[i] = translate_y( win_y[i] );
-			}
-
-System.err.println( "update: win_x0, win_y0: "+win_x0+", "+win_y0 );
-System.err.println( "\twin_width, win_height: "+win_width+", "+win_height );
-System.err.println( "\tvpt_x0, vpt_y0: "+vpt_x0+", "+vpt_y0 );
-System.err.println( "\tvpt_width, vpt_height: "+vpt_width+", "+vpt_height );
-System.err.println( "\tsize: "+size );
-for ( int i = 0; i < x.length; i++ )
-System.err.println( "\tx["+i+"], y["+i+"]: "+x[i]+", "+y[i] );
+if ( p != null ) System.out.println( "\t"+" mean: "+p.expected_value()+", stddev: "+p.sqrt_variance() );
+			set_geometry();
 			repaint();
 		}
 		catch (Exception e)
@@ -172,6 +92,96 @@ System.err.println( "\tx["+i+"], y["+i+"]: "+x[i]+", "+y[i] );
 			e.printStackTrace();
 			throw new RuntimeException( "PlotDistribution.update: "+e );
 		}
+	}
+
+	void set_geometry() throws Exception
+	{
+		if ( p == null ) return;
+
+		double[] support = p.effective_support(1e-3);
+
+		size = getSize();
+		vpt_x0 = 25;
+		vpt_y0 = 25;
+		vpt_width = size.width-2*25;
+		vpt_height = size.height-2*25;
+
+		xmin = support[0];
+		xmax = support[1];
+		xmean = p.expected_value();
+
+		double y_max = -1;
+
+		// Store the points to be plotted in window coordinate;
+		// translate to viewport coords after fudging window parameters.
+		double[] win_x, win_y;
+
+		if ( variable.is_discrete() )
+		{
+			y_max = 1; // set maximum for discrete probabilities
+			int N = 1 + (int) support[1];
+			win_x = new double[N];
+			win_y = new double[N];
+			double[] xi = new double[1];
+
+			for ( int i = 0; i < N; i++ )
+			{
+				xi[0] = i;
+				win_x[i] = xi[0];
+				win_y[i] = p.p( xi );
+			}
+		}
+		else
+		{
+			int N = 100;
+			double x0 = support[0], x1 = support[1];
+			double dx = (x1-x0)/N;
+
+			win_x = new double[N];
+			win_y = new double[N];
+			double[] xi = new double[1];
+			double yi;
+
+			for ( int i = 0; i < N; i++ )
+			{
+				xi[0] = x0 + (i+0.5)*dx;
+				yi = p.p( xi );
+				if ( yi > y_max ) y_max = yi;
+				win_x[i] = xi[0];
+				win_y[i] = yi;
+			}
+		}
+
+		win_x0 = xmin;
+		win_y0 = 0;
+		win_width = xmax-xmin;
+		win_height = y_max;
+
+		// Now fudge the window parameters so that plotted stuff
+		// doesn't touch the edges of the viewport.
+
+		double win_xmargin = win_width*0.1, win_ymargin = win_height*0.1;
+		win_x0 -= win_xmargin;
+		win_y0 -= win_ymargin;
+		win_width += 2*win_xmargin;
+		win_height += 2*win_ymargin;
+
+		// Now translate window coords to viewport coords.
+
+		x = new int[ win_x.length ];
+		y = new int[ win_y.length ];
+
+		for ( int i = 0; i < win_x.length; i++ )
+		{
+			x[i] = translate_x( win_x[i] );
+			y[i] = translate_y( win_y[i] );
+		}
+
+System.err.println( "set_geometry: win_x0, win_y0: "+win_x0+", "+win_y0 );
+System.err.println( "\twin_width, win_height: "+win_width+", "+win_height );
+System.err.println( "\tvpt_x0, vpt_y0: "+vpt_x0+", "+vpt_y0 );
+System.err.println( "\tvpt_width, vpt_height: "+vpt_width+", "+vpt_height );
+System.err.println( "\tsize: "+size );
 	}
 
     public void paint(Graphics g)
@@ -184,7 +194,6 @@ System.err.println( "\tx["+i+"], y["+i+"]: "+x[i]+", "+y[i] );
 		catch (RemoteException e) { throw new RuntimeException( "PlotDistribution.paint: "+e ); }
 
 		if ( x == null ) return; // no data to plot yet
-System.err.println( "paint: x.length: "+x.length+", is_discrete: "+is_discrete );
 
         g.setColor(Color.white);
 		g.fillRect( vpt_x0, vpt_y0, vpt_width, vpt_height );
@@ -207,22 +216,32 @@ System.err.println( "paint: x.length: "+x.length+", is_discrete: "+is_discrete )
 
         g.setColor(Color.darkGray);
         g.drawRect( vpt_x0, vpt_y0, vpt_width, vpt_height );
-		int y0 = translate_y(0);
-		g.drawLine( translate_x(win_x0), y0, translate_x(win_x0+win_width), y0 );
+		int y0 = translate_y(0), y1 = translate_y( -win_height/10.0/2 );
+		g.drawLine( translate_x(xmin), y0, translate_x(xmax), y0 );
+
+		g.drawLine( translate_x(xmin), y0, translate_x(xmin), y1 );
+		g.drawLine( translate_x(xmax), y0, translate_x(xmax), y1 );
+		g.drawLine( translate_x(xmean), y0, translate_x(xmean), y1 );
     }
 
-    public boolean mouseUp(java.awt.Event evt, int x, int y)
+	class PlotComponentListener implements ComponentListener
 	{
-        // RESET SCALE AND TRANSLATION ???
-        repaint();
-        return true;
-    }
+		public void componentResized(ComponentEvent e)
+		{
+			try
+			{
+				set_geometry();
+				repaint();
+			}
+			catch (Exception ee)
+			{
+				ee.printStackTrace();
+				System.err.println( "componentResized: stagger forward." );
+			}
+		}
 
-    public boolean mouseDrag( java.awt.Event evt, int x, int y )
-    {
-        // SET RUBBER BOX ???
-        repaint();
-        return true;
-    }
+		public void componentMoved(ComponentEvent e) {}
+		public void componentShown(ComponentEvent e) {}
+		public void componentHidden(ComponentEvent e) {}
+	}
 }
-

0943c5fcb79fa7b33f8bb12fb2dbc25b21c87045
robert-dodier <robert.dodier@gmail.com>
1999-01-17 23:25:27 +0000

    made flags public (was protexted)

--- riso/riso/regression/SquashingNetwork.java
+++ riso/riso/regression/SquashingNetwork.java
@@ -39,9 +39,9 @@ public class SquashingNetwork implements RegressionModel
 	public final int SHORTCUTS = 2;
 	public final int BATCH_UPDATE = 4;
 	public final int SIGMOIDAL_OUTPUT = 8;
+	public int flags;				// flags for whistles and bells
 
 	protected boolean is_ok;		// was net created successfully?
-	protected int flags;		// flags for whistles and bells
 	protected int nlayers;	// how many layers in net
 	protected int[] unit_count;	// how many units in each layer
 	protected boolean[][] is_connected;	// tell what layer connects to what

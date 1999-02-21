cf1a0d8834777e4ff4755b4e2e6e8bbe1a3cd891
robert-dodier <robert.dodier@gmail.com>
1999-02-21 00:54:15 +0000

    removed is_changed balonery; removed add_interest balonery

--- riso/riso/remote_data/RemoteObservable.java
+++ riso/riso/remote_data/RemoteObservable.java
@@ -4,18 +4,8 @@ import java.rmi.*;
 
 public interface RemoteObservable extends Remote
 {
-	/** Return a list of the items which are contained within this observable.
-	  */
-	public Object[] known_items() throws RemoteException;
-
-	/** Adds an observer to the list of observers watching this observable as a whole
-	  * (and not any particular object within this observable).
-	  */
-	public void add_observer( RemoteObserver o ) throws RemoteException;
-
 	/** Adds an observer to the list of observers watching a particular object, 
-	  * <tt>of_interest</tt>, within
-	  * this observable.
+	  * <tt>of_interest</tt>, within this observable.
 	  */
 	public void add_observer( RemoteObserver o, Object of_interest ) throws RemoteException;
 
@@ -36,9 +26,6 @@ public interface RemoteObservable extends Remote
 	/** Notifies any observers watching the object <tt>of_interest</tt> within this
 	  * observable. 
 	  *
-	  * <p> The item <tt>of_interest</tt> can be a reference to this observable; that means
-	  * that the observer is updated if any item within it has changed.
-	  *
 	  * <p> If an <tt>update</tt> call fails with a <tt>RemoteException</tt>,
 	  * the observer is removed from the list of observers for this observable.
 	  *
@@ -50,9 +37,6 @@ public interface RemoteObservable extends Remote
 	/** Notifies any observers watching the object <tt>of_interest</tt> within this
 	  * observable. The argument sent to the observer in the <tt>update</tt> call is <tt>null</tt>.
 	  *
-	  * <p> The item <tt>of_interest</tt> can be a reference to this observable; that means
-	  * that the observer is updated if any item within it has changed.
-	  *
 	  * <p> If an <tt>update</tt> call fails with a <tt>RemoteException</tt>,
 	  * the observer is removed from the list of observers for this observable.
 	  *
@@ -60,30 +44,10 @@ public interface RemoteObservable extends Remote
 	  */
 	public void notify_observers( Object of_interest ) throws RemoteException;
 
-	/** Notifies observers watching this observable as a whole (i.e., the ones
-	  * which called <tt>add_observer</tt> with <tt>of_interest</tt> equal
-	  * to <tt>this</tt>) and those watching some item within this observable.
-	  * The observers watching the observable as a whole are called last.
-	  *
+	/** Notifies all observers watching something within this observable.
 	  * <p>If an <tt>update</tt> call fails with a <tt>RemoteException</tt>,
 	  * the observer is removed from the list of observers for this observable.
 	  */
 	public void notify_all_observers() throws RemoteException;
-
-	/** Tells whether the item <tt>of_interest</tt> has changed.
-	  */
-	public boolean has_changed( Object of_interest ) throws RemoteException;
-
-	/** Mark the item <tt>of_interest</tt> as "changed."
-	  * Future calls to <tt>has_changed</tt> will return <tt>true</tt> until
-	  * <tt>clear_changed</tt> is called.
-	  */
-	public void set_changed( Object of_interest ) throws RemoteException;
-
-	/** Mark the item <tt>of_interest</tt> as "not changed."
-	  * Future calls to <tt>has_changed</tt> will return <tt>false</tt> until
-	  * <tt>set_changed</tt> is called.
-	  */
-	public void clear_changed( Object of_interest ) throws RemoteException;
 }
 

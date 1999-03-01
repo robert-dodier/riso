ce14c7a793546b8e73b94180f0ecce26296f9369
robert-dodier <robert.dodier@gmail.com>
1999-03-01 22:17:49 +0000

    commented out debug output; fixed slight bug in termination

--- riso/riso/general/MatchClassPattern.java
+++ riso/riso/general/MatchClassPattern.java
@@ -98,28 +98,28 @@ public class MatchClassPattern
 					n = 0;
 					if ( ++ii == sm.length )
 					{
-System.err.println( "reached end of pattern w/ seqc "+seqc.getName() );
+// System.err.println( "reached end of pattern w/ seqc "+seqc.getName() );
 						return false;
 					}
 				}
 				else
 				{
-System.err.println( seqc.getName()+" not instance of "+sm[ii].c.getName()+"; found "+n+" before failing" );
+// System.err.println( seqc.getName()+" not instance of "+sm[ii].c.getName()+"; found "+n+" before failing" );
 					return false;
 				}
 
-System.err.println( "add "+sm[ii].level+" to class-specific score; from "+sm[ii].c.getName() );
+// System.err.println( "add "+sm[ii].level+" to class-specific score; from "+sm[ii].c.getName() );
 			class_specific_score[0] += sm[ii].level;
 
 			if ( sm[ii].c.isAssignableFrom(seqc) )
 			{
 				if ( ++n == sm[ii].reps )
 				{
-System.err.println( "reached end of "+sm[ii].reps+" "+sm[ii].c.getName() );
+// System.err.println( "reached end of "+sm[ii].reps+" "+sm[ii].c.getName() );
 					n = 0;
 					if ( ++ii == sm.length && e.hasMoreElements() )
 					{
-System.err.println( "reached end of pattern w/ seqc "+seqc.getName() );
+// System.err.println( "reached end of pattern w/ seqc "+seqc.getName() );
 						return false;
 					}
 					++count_specific_score[0];
@@ -127,18 +127,24 @@ System.err.println( "reached end of pattern w/ seqc "+seqc.getName() );
 			}
 			else
 			{
-System.err.println( seqc.getName()+" not instance of "+sm[ii].c.getName()+"; found "+n+" before failing" );
+// System.err.println( seqc.getName()+" not instance of "+sm[ii].c.getName()+"; found "+n+" before failing" );
 				return false;
 			}
 		}
 
-System.err.println( "fell out at bottom, ii: "+ii+", n: "+n );
+// System.err.println( "fell out at bottom, ii: "+ii+", n: "+n );
 		if ( ii == sm.length && n == sm[ii-1].reps )
 			++count_specific_score[0];
 
+		if ( ii < sm.length-1 )
+		{
+// System.err.println( "didn't reach end of pattern; ii: "+ii+", sm.length: "+sm.length );
+			return false;
+		}
+
 		if ( ii < sm.length && n != sm[ii].reps && sm[ii].reps != -1 ) 
 		{
-System.err.println( "reached end of pattern w/ n "+n+" (should be "+sm[ii].reps+")" );
+// System.err.println( "reached end of pattern w/ n "+n+" (should be "+sm[ii].reps+")" );
 			return false;
 		}
 

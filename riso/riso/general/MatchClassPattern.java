8135f52e639c2bbb49da414028f597afcf43193b
robert-dodier <robert.dodier@gmail.com>
1999-03-13 17:11:36 +0000

    moved SeqTrple into its own class file

--- riso/riso/general/MatchClassPattern.java
+++ riso/riso/general/MatchClassPattern.java
@@ -1,36 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-class SeqTriple
-{
-	int level, reps;
-	Class c;
-
-	public SeqTriple( String s, int reps )
-	{
-		try
-		{
-			c = Class.forName(s);
-			level = nsuperclasses(c);
-			this.reps = reps;
-		}
-		catch (Exception e) { e.printStackTrace(); System.exit(1); } // !!!
-	}
-
-	public static int nsuperclasses( Class c ) throws ClassNotFoundException
-	{
-		int n = 0;
-		while ( (c = c.getSuperclass()) != null )
-			++n;
-		return n;
-	}
-
-	public String toString()
-	{
-		return "["+level+","+c.getName()+","+reps+"]";
-	}
-}
-
 public class MatchClassPattern
 {
 	// This one remains as an example.

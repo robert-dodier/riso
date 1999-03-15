051ccf1ebc81543f613e127b346571fa66ed296e
robert-dodier <robert.dodier@gmail.com>
1999-03-15 06:05:19 +0000

    added code to reduce the mixture compomemts.

--- riso/riso/distributions/computes_pi/RegressionDensity_MixGaussians.java
+++ riso/riso/distributions/computes_pi/RegressionDensity_MixGaussians.java
@@ -1,5 +1,6 @@
 package riso.distributions.computes_pi;
 import java.rmi.*;
+import java.util.*;
 import riso.distributions.*;
 import SeqTriple;
 
@@ -40,7 +41,54 @@ public class RegressionDensity_MixGaussians implements PiHelper
 		int[] k = new int[ pi.length ], l = new int[1];
 	
 		mix_gaussians_pi_approx_inner_loop( mix, pi, y, k, l, pi.length-1, null );
-		// REDUCE MIXTURE ??? WILL EVENTUALLY WANT THAT !!!
+
+		// Here's an easy step toward simplification: throw out a component if it
+		// appears to be nearly the same as some other component.
+
+		Vector duplicates = new Vector(), duplicated = new Vector();
+
+		for ( i = 0; i < mix.ncomponents(); i++ )
+		{
+			double m_i = mix.components[i].expected_value();
+			double s_i = mix.components[i].sqrt_variance();
+
+			for ( int j = i+1; j < mix.ncomponents(); j++ )
+			{
+				double m_j = mix.components[j].expected_value();
+				double s_j = mix.components[j].sqrt_variance();
+
+				if ( s_i == 0 || s_j == 0 ) continue;
+
+				double dm = Math.abs(m_i-m_j), rs = s_i/s_j, s_ij = Math.sqrt( 1/( 1/(s_i*s_i) + 1/(s_j*s_j) ) );
+
+				if ( dm/s_ij < 2.5e-1 && rs > 1 - 2e-1 && rs < 1 + 2e-1 )
+				{
+					duplicates.addElement( new Integer(i) );
+					duplicated.addElement( new Integer(j) );
+					break; // go on to next i
+				}
+			}
+		}
+
+System.err.println( "Reg_MixG.comptes_pi: remove "+duplicates.size()+" duplicate components." );
+		mix.remove_components( duplicates, duplicated );
+
+		// Here's another easy one:
+		// throw out mixture components which have very small weight.
+
+		final double MIN_MIX_PROPORTION = 5e-3;
+		Vector too_light = new Vector();
+
+		for ( i = 0; i < mix.ncomponents(); i++ )
+		{
+			if ( mix.mix_proportions[i] < MIN_MIX_PROPORTION )
+			{
+				too_light.addElement( new Integer(i) );
+			}
+		}
+
+System.err.println( "Reg_MixG.comptes_pi: remove "+too_light.size()+" too-light components." );
+		mix.remove_components( too_light, null );
 
 		return mix;
 	}

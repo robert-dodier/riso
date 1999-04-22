4bff7785ec329227da5602092bfdcc62dde670d0
robert-dodier <robert.dodier@gmail.com>
1999-04-22 19:46:27 +0000

    now uses SplineApproximation; took out some debug code.

--- riso/riso/distributions/computes_posterior/AbstractDistribution_AbstractDistribution.java
+++ riso/riso/distributions/computes_posterior/AbstractDistribution_AbstractDistribution.java
@@ -1,7 +1,6 @@
 package riso.distributions.computes_posterior;
 import riso.distributions.*;
 import riso.approximation.*;
-import TopDownSplayTree;
 import SeqTriple;
 
 public class AbstractDistribution_AbstractDistribution extends AbstractPosteriorHelper
@@ -23,21 +22,8 @@ public class AbstractDistribution_AbstractDistribution extends AbstractPosterior
 		p_l[1] = lambda;
 		DistributionProduct dp = new DistributionProduct( false, pi instanceof Discrete,  p_l );
 
-		MixGaussians q = dp.initial_mix( pi.effective_support( 1e-6 ) );
-		double tolerance = 1e-5;
-		q = GaussianMixApproximation.do_approximation( (Distribution)dp, q, dp.support, tolerance );
+		SplineDensity q = SplineApproximation.do_approximation( (Distribution)dp, dp.support );
 
-// System.err.println( "compute_posterior: lambda class: "+lambda.getClass() );
-// if ( lambda instanceof riso.distributions.computes_lambda_message.IntegralCache ) {
-// double[][] xy = ((riso.distributions.computes_lambda_message.IntegralCache)lambda).cache.dump();
-// double[] x = new double[1];
-// System.err.println( "dump: (lambda eval at) (lambda eval) (dist prod eval) (approx eval)" );
-// for ( int i = 0; i < xy.length; i++ ) {
-// x[0] = xy[i][0];
-// System.err.println( xy[i][0]+"\t"+xy[i][1]+"\t"+dp.p(x)+"\t"+q.p(x) ); } }
-		if ( q.ncomponents() == 1 )
-			return q.components[0];
-		else
-			return q;
+		return q;
 	}
 }

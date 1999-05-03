b54da0f2dabee429cd6263d4fcc0494ac894d1e6
robert-dodier <robert.dodier@gmail.com>
1999-05-03 04:13:28 +0000

    Initial revision

---
+++ riso/riso/distributions/computes_pi/Identity_Distribution.java
@@ -0,0 +1,48 @@
+/* RISO: an implementation of distributed belief networks.
+ * Copyright (C) 1999, Robert Dodier.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA, 02111-1307, USA,
+ * or visit the GNU web site, www.gnu.org.
+ */
+package riso.distributions.computes_pi;
+import java.util.*;
+import riso.distributions.*;
+import SeqTriple;
+
+/** @see PiHelper
+  */
+public class Identity_Distribution implements PiHelper
+{
+	/** Returns a description of the sequences of distributions accepted
+	  * by this helper -- namely one <tt>Identity</tt>
+	  * followed by one <tt>AbstractDistribution</tt>.
+	  */
+	public static SeqTriple[] description()
+	{
+		SeqTriple[] s = new SeqTriple[2];
+		s[0] = new SeqTriple( "riso.distributions.Identity", 1 );
+		s[1] = new SeqTriple( "riso.distributions.AbstractDistribution", 1 );
+		return s;
+	}
+
+	/** For a child variable with an <tt>Identity</tt> conditional distribution,
+	  * the upstream support is just the same as the pi message that comes from the
+	  * sole parent.
+	  */
+	public Distribution compute_pi( ConditionalDistribution py_in, Distribution[] pi_messages ) throws Exception
+	{
+		return pi_messages[0];
+	}
+}

b10a88144ced462287daa72c6d1ed694652078c6
robert-dodier <robert.dodier@gmail.com>
1999-05-05 04:52:55 +0000

    rearranged the program pretty heavily -- split off a separate function to compute convolution.

--- riso/riso/numerical/Convolve.java
+++ riso/riso/numerical/Convolve.java
@@ -2,15 +2,20 @@ package numerical;
 import java.io.*;
 import SmarterTokenizer;
 
+/** This class contains a method to compute the discrete convolution of two
+  * sequences.
+  */
 public class Convolve
 {
-	/** Read two columns of real data, convolve them, and print the result.
+	/** Compute the discrete convolution of two sequences of data. 
+	  * First <tt>-Nx</tt> data are read for the first sequence, then <tt>-Ny</tt> data
+	  * are read for the second.
 	  */
 	public static void main( String[] args )
 	{
 		try
 		{
-			int N = 0;
+			int Nx = 0, Ny = 0;
 
 			for ( int i = 0; i < args.length; i++ )
 			{
@@ -18,77 +23,94 @@ public class Convolve
 				switch (args[i].charAt(1))
 				{
 				case 'N':
-					N = Format.atoi( args[++i] );
+					switch (args[i].charAt(2))
+					{
+					case 'x':
+						Nx = Format.atoi( args[++i] );
+						break;
+					case 'y':
+						Ny = Format.atoi( args[++i] );
+						break;
+					}
 					break;
 				}
 			}
 
 			SmarterTokenizer st = new SmarterTokenizer( new InputStreamReader( System.in ) );
 
-			int Npadded;
-			if ( (1 << FFT.ilog2(N)) == N )
-				// N is a power of two; number of padding zeros will be equal to N.
-				Npadded = 2*N;
-			else
-				// N is not a power of two; double N, then round up to next power of 2.
-				Npadded = 1 << (FFT.ilog2(2*N)+1);
+			double[] x = new double[Nx], y = new double[Ny];
 
-			Complex[] x = new Complex[ Npadded ];
-
-			for ( int i = 0; i < N; i++ )
+			for ( int i = 0; i < Nx; i++ )
 			{
-				x[i] = new Complex();
-
 				st.nextToken();
-				x[i].real = Format.atof( st.sval );
+				x[i] = Format.atof( st.sval );
+			}
+
+			for ( int i = 0; i < Ny; i++ )
+			{
 				st.nextToken();
-				x[i].imag = Format.atof( st.sval );
+				y[i] = Format.atof( st.sval );
 			}
 
-			for ( int i = N; i < Npadded; i++ ) x[i] = new Complex();
+			double[] cxy = convolve(x,y);
 
-			FFT.fft( x, Npadded );		// compute FFT on both columns at once.
-
-System.err.println( "FFT(x):" );
-for ( int i = 0; i < Npadded; i++ ) System.err.println( x[i].real+"  "+x[i].imag );
-
-			// Compute product of FFT's of each column of data.
-			// First reconstruct the FFT coefficients from FFT of both columns,
-			// then multiply.
-
-			Complex[] xx = new Complex[ Npadded ];
-
-System.err.println( "\n"+"untangle two FFT's:" );
-			for ( int i = 0; i < Npadded; i++ )
-			{
-				// The FFT coefficient of the first column is (R1,I1), and 
-				// that of the second is (R2,-I2).
-				
-				int i_reflect = (i == 0? 0: Npadded-i);
-
-				double R1 = (x[i].real + x[i_reflect].real)/2;
-				double I1 = (x[i].imag - x[i_reflect].imag)/2;
-				double R2 = (x[i].imag + x[i_reflect].imag)/2;
-				double I2 = (x[i].real - x[i_reflect].real)/2;
-System.err.println( "("+R1+","+I1+"), ("+R2+","+(-I2)+")" );
-
-				// Multiply the coefficients to obtain the convolution.
-				xx[i] = new Complex();
-				xx[i].real = R1*R2 + I1*I2;
-				xx[i].imag = -R1*I2 + I1*R2;
-			}
-
-System.err.println( "\n"+"product of two FFT's:" );
-for ( int i = 0; i < Npadded; i++ ) System.err.println( xx[i].real+"  "+xx[i].imag );
-
-			FFT.invfft( xx, Npadded );
-
-System.err.println( "\n"+"convolution:" );
-			for ( int i = 0; i < 2*N-1; i++ )
-			{
-				System.out.println( xx[i].real+"  "+xx[i].imag );
-			}
+			System.out.println( "length of convolution: "+cxy.length );
+			for ( int i = 0; i < cxy.length; i++ )
+				System.out.println( cxy[i] );
 		}
 		catch (Exception e) { e.printStackTrace(); }
+	}
+
+	/** Compute convolution of two sequences. The length of the two sequences need not be
+	  * the same, and the lengths need not be powers of two. The length of the output array
+	  * will be the length of <tt>x</tt> plus the length of <tt>y</tt>, less one.
+	  */
+	public static double[] convolve( double[] x, double[] y )
+	{
+		int Npadded = x.length+y.length, N = x.length+y.length-1;
+
+		if ( (1 << FFT.ilog2(Npadded)) != Npadded )
+			// Npadded is not a power of two; round up to next power of 2.
+			Npadded = 1 << (FFT.ilog2(Npadded)+1);
+
+		Complex[] xy = new Complex[ Npadded ];
+
+		for ( int i = 0; i < Npadded; i++ ) xy[i] = new Complex();
+		for ( int i = 0; i < x.length; i++ ) xy[i].real = x[i];
+		for ( int i = 0; i < y.length; i++ ) xy[i].imag = y[i];
+
+		FFT.fft(xy);		// compute FFT on both columns at once.
+
+		// Compute product of FFT's of each column of data.
+
+		Complex[] xx = new Complex[ Npadded ];
+
+		for ( int i = 0; i < Npadded; i++ )
+		{
+			// The FFT coefficient of the first column is (R1,I1), and 
+			// that of the second is (R2,-I2).
+			
+			int i_reflect = (i == 0? 0: Npadded-i);
+
+			double R1 = (xy[i].real + xy[i_reflect].real)/2;
+			double I1 = (xy[i].imag - xy[i_reflect].imag)/2;
+			double R2 = (xy[i].imag + xy[i_reflect].imag)/2;
+			double I2 = (xy[i].real - xy[i_reflect].real)/2;
+
+			// Multiply the coefficients to obtain the convolution.
+
+			xx[i] = new Complex();
+			xx[i].real = R1*R2 + I1*I2;
+			xx[i].imag = -R1*I2 + I1*R2;
+		}
+
+		FFT.invfft(xx);	// inverse transform to obtain convolution.
+
+		double[] cxy = new double[N];
+
+		for ( int i = 0; i < cxy.length; i++ )
+			cxy[i] = xx[i].real;	// imaginary part is always zero.
+
+		return cxy;
 	}
 }

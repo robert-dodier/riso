77eb535d6e9c14164dea275eb9e948ae625b6ff2
robert-dodier <robert.dodier@gmail.com>
1999-05-11 23:24:10 +0000

    revised the security handler so that it prevents file i/o

--- riso/riso/general/RemoteApp.java
+++ riso/riso/general/RemoteApp.java
@@ -33,7 +33,7 @@ public class RemoteApp
 
 		try
 		{
-			System.setSecurityManager( new NullSecurityManager() );
+			System.setSecurityManager( new RemoteAppSecurityManager() );
 			Class c;
 
 			if ( url != null )
@@ -69,35 +69,30 @@ public class RemoteApp
 	}
 }
 
-class NullSecurityManager extends SecurityManager {
-    public void checkCreateClassLoader() { } 
-    public void checkAccess(Thread g) { }
-    public void checkAccess(ThreadGroup g) { }
-    public void checkExit(int status) { }
-    public void checkExec(String cmd) { }
-    public void checkLink(String lib) { }
-    public void checkRead(FileDescriptor fd) { }
-    public void checkRead(String file) { }
-    public void checkRead(String file, Object context) { }
-    public void checkWrite(FileDescriptor fd) { }
-    public void checkWrite(String file) { }
-    public void checkDelete(String file) { }
-    public void checkConnect(String host, int port) { }
-    public void checkConnect(String host, int port, Object context) { }
-    public void checkListen(int port) { }
-    public void checkAccept(String host, int port) { }
-    public void checkMulticast(InetAddress maddr) { }
-    public void checkMulticast(InetAddress maddr, byte ttl) { }
-    public void checkPropertiesAccess() { }
-    public void checkPropertyAccess(String key) { }
-    public void checkPropertyAccess(String key, String def) { }
-    public boolean checkTopLevelWindow(Object window) { return true; }
-    public void checkPrintJobAccess() { }
-    public void checkSystemClipboardAccess() { }
-    public void checkAwtEventQueueAccess() { }
-    public void checkPackageAccess(String pkg) { }
-    public void checkPackageDefinition(String pkg) { }
-    public void checkSetFactory() { }
-    public void checkMemberAccess(Class clazz, int which) { }
-    public void checkSecurityAccess(String provider) { }
+class RemoteAppSecurityManager extends RMISecurityManager
+{
+    public void checkAccess(Thread g) {}		// loaded classes are allowed to use threads
+    public void checkAccess(ThreadGroup g) {}
+    public void checkExit(int status) {} 		// loaded classes can exit the VM
+
+    // ALLOW ??? public void checkLink(String lib) { }
+
+    public void checkPropertiesAccess() {}			// loaded classes can access properties
+    public void checkPropertyAccess(String key) {}
+    public void checkPropertyAccess(String key, String def) {}
+
+    public void checkConnect(String host, int port) {}
+    public void checkConnect(String host, int port, Object context) {}
+    public void checkListen(int port) {}
+    public void checkAccept(String host, int port) {}
+    public void checkMulticast(InetAddress maddr) {}
+    public void checkMulticast(InetAddress maddr, byte ttl) {}
+    // ??? public void checkPrintJobAccess() { }
+    // ??? public void checkSystemClipboardAccess() { }
+    // ??? public void checkAwtEventQueueAccess() { }
+    // ??? public void checkPackageAccess(String pkg) { }
+    // ??? public void checkPackageDefinition(String pkg) { }
+    // ??? public void checkSetFactory() { }
+    // ??? public void checkMemberAccess(Class clazz, int which) { }
+    // ??? public void checkSecurityAccess(String provider) { }
 }	

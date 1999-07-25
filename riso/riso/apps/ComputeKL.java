a043b804dc592fd1c6ba1ecc2efb8573f9d92172
robert-dodier <robert.dodier@gmail.com>
1999-07-25 22:32:51 +0000

    now takes 2 or 3 cmd line args -- if 2, just compute KL(p1,p2)

--- riso/riso/apps/ComputeKL.java
+++ riso/riso/apps/ComputeKL.java
@@ -28,6 +28,7 @@ import numerical.*;
   */
 public class ComputeKL
 {
+	public boolean verbose = false;
 	IntegralHelper1d ceih, eih;
 
 	public ComputeKL( Distribution p1, Distribution p2 ) throws Exception
@@ -58,7 +59,7 @@ public class ComputeKL
 			throw new Exception( "ComputeKL.do_compute_kl: attempt failed; "+ex );
 		}
 
-// System.err.println( "ComputeKL.do_compute_kl: entropy: "+e+"  cross-entropy: "+ce );
+		if ( verbose ) System.err.println( "ComputeKL.do_compute_kl: entropy: "+e+"  cross-entropy: "+ce );
 		return ce-e;
 	}
 
@@ -66,31 +67,45 @@ public class ComputeKL
 	{
 		try
 		{
-			String x_fullname = args[0], e_fullname = args[1], evalue_string = args[2];
+			String x_fullname = args[0], e_fullname = args[1];
 			String x_name = x_fullname.substring( x_fullname.indexOf(".")+1 );
 			String e_name = e_fullname.substring( e_fullname.indexOf(".")+1 );
 
 System.err.println( "x_fullname: "+x_fullname+", e_fullname: "+e_fullname );
 System.err.println( "x_name: "+x_name+", e_name: "+e_name );
 
-			double evalue = numerical.Format.atof(evalue_string);
-System.err.println( "evalue: "+evalue );
-
 			BeliefNetworkContext bnc = new BeliefNetworkContext(null);
+
 			AbstractBeliefNetwork xbn = (AbstractBeliefNetwork) bnc.get_reference( NameInfo.parse_variable(x_fullname,bnc) );
 			AbstractBeliefNetwork ebn = (AbstractBeliefNetwork) bnc.get_reference( NameInfo.parse_variable(e_fullname,bnc) );
 			AbstractVariable x = (AbstractVariable) xbn.name_lookup(x_name);
 			AbstractVariable e = (AbstractVariable) ebn.name_lookup(e_name);
 
-			ebn.clear_posterior(e);
-			Distribution px = xbn.get_posterior(x);
+			if ( args.length == 3 )
+			{
+				String evalue_string = args[2];
+				double evalue = numerical.Format.atof(evalue_string);
+System.err.println( "evalue: "+evalue );
+
+				ebn.clear_posterior(e);
+				Distribution px = xbn.get_posterior(x);
 System.err.println( "px: "+px.format_string("") );
-			ebn.assign_evidence(e,evalue);
-			Distribution pxe = xbn.get_posterior(x);
+				ebn.assign_evidence(e,evalue);
+				Distribution pxe = xbn.get_posterior(x);
 System.err.println( "pxe: "+pxe.format_string("") );
 
-			ComputeKL kl_doer = new ComputeKL( pxe, px );	// OTHER WAY AROUND ???
-			System.err.println( "ComputeKL: KL == "+kl_doer.do_compute_kl() );
+				ComputeKL kl_doer = new ComputeKL( pxe, px );	// OTHER WAY AROUND ???
+				kl_doer.verbose = true;
+				System.err.println( "ComputeKL: KL == "+kl_doer.do_compute_kl() );
+			}
+			else
+			{
+				Distribution px = xbn.get_posterior(x);
+				Distribution pe = ebn.get_posterior(e);
+				ComputeKL kl_doer = new ComputeKL( px, pe );
+				kl_doer.verbose = true;
+				System.err.println( "ComputeKL: KL == "+kl_doer.do_compute_kl() );
+			}
 		}
 		catch (Exception e) { e.printStackTrace(); }
 		System.exit(0);

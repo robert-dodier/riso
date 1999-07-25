7e89766e40675ef28456b537758b05930a8b9428
robert-dodier <robert.dodier@gmail.com>
1999-07-25 14:39:01 +0000

    various mods, seems to work

--- riso/riso/apps/ComputeKL.java
+++ riso/riso/apps/ComputeKL.java
@@ -18,6 +18,7 @@
  */
 package riso.approximation;
 import java.rmi.*;
+import riso.belief_nets.*;
 import riso.distributions.*;
 import numerical.*;
 
@@ -29,15 +30,14 @@ public class ComputeKL
 {
 	IntegralHelper1d ceih, eih;
 
-	public ComputeKL( Distribution p1, Distribution p2 )
+	public ComputeKL( Distribution p1, Distribution p2 ) throws Exception
 	{
         CrossEntropyIntegrand cei = new CrossEntropyIntegrand( p1, p2 );
 		EntropyIntegrand ei = new EntropyIntegrand( p1 );
 
 		double[][] support = new double[1][];
 
-		try { support[0] = p1.effective_support( 1e-4 ); }
-		catch (RemoteException e) { throw new RuntimeException( "ComputeKL: attempt to construct helper failed; "+e ); }
+		support[0] = p1.effective_support( 1e-4 );
 
 		boolean is_discrete = p1 instanceof Discrete;
 		ceih = new IntegralHelper1d( cei, support, is_discrete );
@@ -58,7 +58,41 @@ public class ComputeKL
 			throw new Exception( "ComputeKL.do_compute_kl: attempt failed; "+ex );
 		}
 
-System.err.println( "ComputeKL.do_compute_kl: entropy: "+e+"  cross-entropy: "+ce );
+// System.err.println( "ComputeKL.do_compute_kl: entropy: "+e+"  cross-entropy: "+ce );
 		return ce-e;
+	}
+
+	public static void main( String[] args )
+	{
+		try
+		{
+			String x_fullname = args[0], e_fullname = args[1], evalue_string = args[2];
+			String x_name = x_fullname.substring( x_fullname.indexOf(".")+1 );
+			String e_name = e_fullname.substring( e_fullname.indexOf(".")+1 );
+
+System.err.println( "x_fullname: "+x_fullname+", e_fullname: "+e_fullname );
+System.err.println( "x_name: "+x_name+", e_name: "+e_name );
+
+			double evalue = numerical.Format.atof(evalue_string);
+System.err.println( "evalue: "+evalue );
+
+			BeliefNetworkContext bnc = new BeliefNetworkContext(null);
+			AbstractBeliefNetwork xbn = (AbstractBeliefNetwork) bnc.get_reference( NameInfo.parse_variable(x_fullname,bnc) );
+			AbstractBeliefNetwork ebn = (AbstractBeliefNetwork) bnc.get_reference( NameInfo.parse_variable(e_fullname,bnc) );
+			AbstractVariable x = (AbstractVariable) xbn.name_lookup(x_name);
+			AbstractVariable e = (AbstractVariable) ebn.name_lookup(e_name);
+
+			ebn.clear_posterior(e);
+			Distribution px = xbn.get_posterior(x);
+System.err.println( "px: "+px.format_string("") );
+			ebn.assign_evidence(e,evalue);
+			Distribution pxe = xbn.get_posterior(x);
+System.err.println( "pxe: "+pxe.format_string("") );
+
+			ComputeKL kl_doer = new ComputeKL( pxe, px );	// OTHER WAY AROUND ???
+			System.err.println( "ComputeKL: KL == "+kl_doer.do_compute_kl() );
+		}
+		catch (Exception e) { e.printStackTrace(); }
+		System.exit(0);
 	}
 }

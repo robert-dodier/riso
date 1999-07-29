d4b9c049cd3fccffb40463bd119faf62f1890946
robert-dodier <robert.dodier@gmail.com>
1999-07-29 15:30:37 +0000

    added code to convert single Gaussians to mixtures (based on a unit gaussian approximation)

--- riso/riso/distributions/computes_pi/RegressionDensity_Gaussian.java
+++ riso/riso/distributions/computes_pi/RegressionDensity_Gaussian.java
@@ -25,6 +25,27 @@ import SeqTriple;
   */
 public class RegressionDensity_Gaussian implements PiHelper
 {
+	/** Gaussian mixture approximation to unit gaussian.
+	  */
+	public static MixGaussians unit_mix = new MixGaussians( 1, 3 );
+
+	static 
+	{
+		// From unit-gaussian-3-approx.mix:
+		// CROSS ENTROPY[9]: 1.419791403766266; target entropy: 1.4189302861251774
+		// effective support of target: -3.90625, 3.90625
+		// effective support of approx: -3.876185771377721, 3.8761857713777204
+		// equivalent sample size: 10,000
+
+		unit_mix.mix_proportions[0] = 0.288272103409951;
+		unit_mix.mix_proportions[1] =  0.4234557931800979;
+		unit_mix.mix_proportions[2] =  0.288272103409951;
+
+		unit_mix.components[0] = new Gaussian( -0.9418690940140162, 0.7511850694051084 );
+		unit_mix.components[1] = new Gaussian( 4.672385040934143E-17, 0.6167460137184869 );
+		unit_mix.components[2] = new Gaussian(  0.9418690940140164, 0.7511850694051082 );
+	}
+
 	/** Returns a description of the sequences of distributions accepted
 	  * by this helper -- namely one <tt>RegressionDensity</tt>
 	  * followed by any number of <tt>Gaussian</tt>.
@@ -37,13 +58,37 @@ public class RegressionDensity_Gaussian implements PiHelper
 		return s;
 	}
 
-	public Distribution compute_pi( ConditionalDistribution y_in, Distribution[] pi ) throws Exception
+	public Distribution compute_pi( ConditionalDistribution py, Distribution[] pi_msgs ) throws Exception
 	{
-		int i, j, k;
+		MixGaussians[] mix_msgs = new MixGaussians[ pi_msgs.length ];
+		
+		for ( int i = 0; i < pi_msgs.length; i++ )
+			mix_msgs[i] = gaussian_to_mix( (Gaussian) pi_msgs[i] );
 
-		RegressionDensity y = (RegressionDensity) y_in;
+		return (new RegressionDensity_MixGaussians()).compute_pi0( py, mix_msgs );
+	}
 
-		return one_gaussian_pi_approx( pi, y );
+	public static MixGaussians gaussian_to_mix( Gaussian g ) throws Exception
+	{
+		double m = g.expected_value(), s = g.sqrt_variance();
+
+		if ( s == 0 ) // it's a delta
+		{
+			MixGaussians mix1 = new MixGaussians(1,1);
+			mix1.mix_proportions[0] = 1;
+			mix1.components[0] = g;
+			return mix1;
+		}
+
+		MixGaussians mix = new MixGaussians( 1, unit_mix.ncomponents() );
+		for ( int i = 0; i < unit_mix.ncomponents(); i++ )
+		{
+			double m0 = unit_mix.components[i].expected_value(), s0 = unit_mix.components[i].sqrt_variance();
+			mix.components[i] = new Gaussian( m+s*m0, s*s0 );
+			mix.mix_proportions[i] = unit_mix.mix_proportions[i];
+		}
+
+		return mix;
 	}
 
 	public static Gaussian one_gaussian_pi_approx( Distribution[] pi, RegressionDensity y ) throws Exception

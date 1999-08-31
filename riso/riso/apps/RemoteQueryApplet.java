651604509d0341a39af2659474d02dea9b24dc32
robert-dodier <robert.dodier@gmail.com>
1999-08-31 17:02:08 +0000

    added some code to list bn's on a host; still working on it

--- riso/riso/apps/RemoteQueryApplet.java
+++ riso/riso/apps/RemoteQueryApplet.java
@@ -21,10 +21,61 @@ import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.rmi.*;
 import SmarterTokenizer;
+
+class RemoteQueryHostFrame extends Frame
+{
+	TextField host_input = new TextField(80);
+	List bn_list = new List();
+
+	RemoteQueryHostFrame( String title )
+	{
+		super(title);
+
+		GridBagLayout gbl = new GridBagLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		setLayout(gbl);
+
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.insets = new Insets( 15, 15, 15, 15 );
+		gbl.setConstraints( host_input, gbc );
+		add(host_input);
+
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weighty = 1;
+		gbl.setConstraints( bn_list, gbc );
+		add(bn_list);
+	}
+
+	public boolean keyDown( Event e, int key )
+	{
+		if ( key == Event.ENTER )
+		{
+			try
+			{
+				String host_name = host_input.getText();
+				String[] names = Naming.list( "rmi://"+host_name );
+				for ( int i = 0; i < names.length; i++ )
+					bn_list.add( names[i] );
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+				System.err.println( "RemoteQueryHostFrame.keyDown: stagger forward." );
+			}
+		}
+
+		return false;
+	}
+}
 
 public class RemoteQueryApplet extends Applet
 {
+	Frame host_frame = new RemoteQueryHostFrame( "Select Host and Belief Network" );
 	TextField input = new TextField(128);
 	TextArea output = new TextArea( "", 10, 128 );
 	PrintStream textarea_pstream = new PrintStream( new TextAreaOutputStream( output ) );
@@ -51,6 +102,8 @@ public class RemoteQueryApplet extends Applet
 		add(output);
 
 		textarea_pstream.println( "RISO Remote Query Applet." );
+
+		host_frame.show();
 	}
 
 	public boolean keyDown( Event e, int key )

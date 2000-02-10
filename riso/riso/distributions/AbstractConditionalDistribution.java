58df34d95619609c93144099642861d249f9f6f3
robert-dodier <robert.dodier@gmail.com>
2000-02-10 20:09:25 +0000

    added clone and pretty_output and parse_string.

--- riso/riso/distributions/AbstractConditionalDistribution.java
+++ riso/riso/distributions/AbstractConditionalDistribution.java
@@ -21,6 +21,7 @@ import java.io.*;
 import java.rmi.*;
 import java.rmi.server.*;
 import riso.belief_nets.*;
+import SmarterTokenizer;
 
 /** Abstract base class for conditional distributions.
   * This class implements only a few methods; most of the methods from the
@@ -48,6 +49,68 @@ public abstract class AbstractConditionalDistribution implements ConditionalDist
 	public void set_variable( Variable x ) { associated_variable = x; }
 
 	/** Return a copy of this conditional distribution.
+	  * This implementation copies the <tt>associated_variable</tt> reference (i.e., the
+	  * reference is not cloned).
+	  * A subclass <tt>clone</tt> method should call this one.
+	  * The method should look like this:
+	  * <pre>
+	  * public Object clone() throws CloneNotSupportedException
+	  * {
+	  *     Subclass copy = (Subclass) super.clone();
+	  *	[subclass-specific code goes here]
+	  *     return copy;
+	  * }
+	  * </pre>
 	  */
-	public abstract Object clone() throws CloneNotSupportedException;
+	public Object clone() throws CloneNotSupportedException 
+	{
+		try
+		{
+			AbstractConditionalDistribution copy = (AbstractConditionalDistribution) this.getClass().newInstance();
+			copy.associated_variable = this.associated_variable;
+			return copy;
+		}
+		catch (Exception e) { throw new CloneNotSupportedException( this.getClass().getName()+".clone failed; nested: "+e ); }
+	}
+
+
+	/** Parse a string containing a description of an instance of this distribution.
+	  * The description is contained within curly braces, which are included in the string.
+	  *
+	  * <p> This method forms a tokenizer from the string and passes off the job to
+	  * <tt>pretty_input</tt>. 
+	  * This implementation is sufficient for many derived classes.
+	  */
+	public void parse_string( String description ) throws IOException
+	{
+		SmarterTokenizer st = new SmarterTokenizer( new StringReader( description ) );
+		pretty_input( st );
+	}
+
+	/** Read an instance of this distribution from an input stream.
+	  * This is intended for input from a human-readable source; this is
+	  * different from object serialization.
+	  *
+	  * <p> In this abstract base class, no implementation of <tt>pretty_input</tt> is given.
+	  *
+	  * @param st Stream tokenizer to read from.
+	  * @throws IOException If the attempt to read the model fails.
+	  */
+	public abstract void pretty_input( SmarterTokenizer st ) throws IOException;
+
+	/** Write an instance of this distribution to an output stream.
+	  * This method forms a <tt>PrintStream</tt> from the <tt>OutputStream</tt> given,
+	  * and passess off the job to <tt>format_string</tt>. 
+	  * This implementation is sufficient for many derived classes.
+	  *
+	  * @param os The output stream to print on.
+	  * @param leading_ws Since the representation is only one line of output, 
+	  *   this argument is ignored.
+	  * @throws IOException If the output fails; this is possible, but unlikely.
+	  */
+	public void pretty_output( OutputStream os, String leading_ws ) throws IOException
+	{
+		PrintStream dest = new PrintStream( new DataOutputStream(os) );
+		dest.print( format_string( leading_ws ) );
+	}
 }

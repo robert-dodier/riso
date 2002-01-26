dcaaffa957efddf054a38debe693dc67ad1f7567
robert-dodier <robert.dodier@gmail.com>
2002-01-26 22:35:13 +0000

    added copyright comment header

--- riso/riso/numerical/MonotoneSpline.java
+++ riso/riso/numerical/MonotoneSpline.java
@@ -1,7 +1,31 @@
+/* RISO: an implementation of distributed belief networks.
+ * Copyright (C) 1999, Robert Dodier.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA, 02111-1307, USA,
+ * or visit the GNU web site, www.gnu.org.
+ */
 package riso.numerical;
 import java.io.*;
 import riso.general.*;
 
+/** An instance of this class represents a monotone cubic spline,
+  * that is, a cubic spline which is either increasing or decreasing between knots.
+  * Formulas are given in F.N. Fritsch and J. Butland,
+  * "A method for constructing local monotone piecewise cubic interpolants",
+  * SIAM J. Scientific and Statistical Computation, 5(2):300--304, 1984.
+  */
 public class MonotoneSpline implements Callback_1d, Serializable
 {
 	int icache = 0;

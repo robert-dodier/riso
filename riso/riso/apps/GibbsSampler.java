dfe918e1d9bb51efc7cf720a2cc6ed1e4724050b
robert-dodier <robert.dodier@gmail.com>
2004-03-08 00:50:19 +0000

    Initial revision

---
+++ riso/riso/apps/GibbsSampler.java
@@ -0,0 +1,80 @@
+/* RISO: an implementation of distributed belief networks.
+ * Copyright (C) 2004, Robert Dodier.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA, 02111-1307, USA,
+ * or visit the GNU web site, www.gnu.org.
+ */
+
+package riso.apps;
+
+import java.rmi.*;
+import riso.belief_nets.*;
+import riso.distributions.*;
+import riso.approximation.*;
+import riso.numerical.*;
+
+public class GibbsSampler
+{
+	public static void main( String[] args )
+	{
+		try
+		{
+			String bn_name = "", host_name = "localhost";
+            int n = 1;
+
+            for ( int i = 0; i < args.length; i++ )
+            {
+                switch (args[i][1])
+                {
+                case 'b':
+                    bn_name = args[++i];
+                    break;
+                case 'h':
+                    host_name = args[++i];
+                    break;
+                case 'n':
+                    n = Integer.parseInt (args[++i]);
+                    break;
+                }
+            }
+
+			Remote remote = Naming.lookup( "rmi://"+host_name+"/"+bn_name );
+			bn = (AbstractBeliefNetwork) remote;
+			
+            AbstractVariable[] all_variables = bn.get_variables();
+            for ( int i = 0; i < all_variables.length; i++ )
+                if ( all_variables[i].is_evidence() )
+                    
+			for ( int i = 0; i < n; i++ )
+			{
+                for ( int j = 0; j < nonevidence.length; i++ )
+                {
+                    AbstractVariable x = nonevidence[j];
+
+                    x.clear_value();
+                    Distribution p = bn.compute_posterior(x);
+                    double[] v = p.random();
+                    bn.assign_evidence(x, v[0]);
+                    System.out.print(" "+v[0]);
+                }
+
+                System.out.print("\n");
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}

5b00a05b12dabb3468b80f867d5536411dae5643
robert-dodier <robert.dodier@gmail.com>
2004-04-29 04:06:45 +0000

    > >RCS file: /home/robert/cdrom-20040427/riso/./distributions/computes_pi/RCS/IndexedDistribution_Discrete.java,v >Working file: /home/robert/cdrom-20040427/riso/./distributions/computes_pi/IndexedDistribution_Discrete.java >head: 1.7 >branch: >locks: strict >access list: >symbolic names: >keyword substitution: kv >total revisions: 8;	selected revisions: 8 >description: >cp ~/cdrom-20040329/riso/distributions/computes_pi/IndexedDistribution_Discrete.java >---------------------------- >revision 1.7 >date: 2004/04/20 16:53:46;  author: robert;  state: Exp;  lines: +24 -4 >added code to flatten ii into i_linear; >changed type of ii from double[] to int[] >---------------------------- >revision 1.6 >date: 2004/04/12 02:07:55;  author: robert;  state: Exp;  lines: +17 -6 >moved debug output into "if (riso.belief_nets.Global.debug > 0) ..." >---------------------------- >revision 1.5 >date: 2004/04/04 00:07:57;  author: robert;  state: Exp;  lines: +37 -57 >substantial rewrite -- let's see if that helps. >---------------------------- >revision 1.4 >date: 2004/04/03 23:13:18;  author: robert;  state: Exp;  lines: +9 -0 >added more debug output >---------------------------- >revision 1.3 >date: 2004/04/03 19:11:20;  author: robert;  state: Exp;  lines: +1 -0 >branches:  1.3.1; >put in reminder to use non_index list >---------------------------- >revision 1.2 >date: 2004/04/03 00:48:39;  author: robert;  state: Exp;  lines: +42 -18 >revised to handle components of IndexedDistribution which are conditional distributions; >strip out pi messages from indexed variables and pass the rest on to the contained cpd. >---------------------------- >revision 1.1 >date: 2004/04/02 21:07:46;  author: robert;  state: Exp; >Initial revision >---------------------------- >revision 1.3.1.1 >date: 2004/04/03 21:59:53;  author: robert;  state: Exp;  lines: +13 -9 >attempted to fix problem in posterior computed for pir1-status: >always comes out (0.5, 0.5), apparently because the values of the index >variable is always 0. tried to use get_density(ii) to fix that. >still broken, my guess is that this is a dead end. >=============================================================================

--- riso/riso/distributions/computes_pi/IndexedDistribution_Discrete.java
+++ riso/riso/distributions/computes_pi/IndexedDistribution_Discrete.java
@@ -39,6 +39,8 @@ public class IndexedDistribution_Discrete implements PiHelper
 		return s;
 	}
 
+    class component_pair { Distribution upd; double alpha; }
+
 	/** All incoming pi-messages are discrete, so this node must not have any
 	  * continuous parents. Thus the pi distribution for this node is simply
 	  * a mixture of the components, with the mixture coefficients equal to
@@ -50,75 +52,118 @@ public class IndexedDistribution_Discrete implements PiHelper
 	  */
 	public Distribution compute_pi( ConditionalDistribution py_in, Distribution[] pi_messages ) throws Exception
 	{
+if (riso.belief_nets.Global.debug > 0)
+{
+    System.err.println ("IndexedDistribution_Discrete.compute_pi: pi_messages: ");
+    for (int i = 0; i < pi_messages.length; i++)
+        System.err.print (pi_messages[i] != null ? pi_messages[i].format_string("") : "(null)");
+}
+
 		IndexedDistribution py = (IndexedDistribution) py_in;
 		py.check_components();
 
 		int m = 0;
-		int[] ii = new int[ pi_messages.length ];
-		int nnonzero = count_nonzero( pi_messages, ii, m );
+		int[] ii = new int[ py.indexes.length ];
+        Vector components = new Vector();
 
-		Mixture pye = new Mixture( 1, nnonzero );
-		ii = new int[ pi_messages.length ];
-		int[] nmix = new int[1], npy = new int[1];
-		assign_components( pi_messages, ii, m, py, pye, nmix, npy );
+		assign_components (pi_messages, ii, m, py, components);
+
+		Mixture pye = new Mixture (1, components.size());
+        for (int i = 0; i < components.size(); i++)
+        {
+            component_pair cp = (component_pair) components.elementAt(i);
+            pye.components[i] = cp.upd;
+            pye.mix_proportions[i] = cp.alpha;
+        }
 
 		pye = Mixture.flatten(pye);
 
 		try { pye = MixGaussians.convert_mixture(pye); }
 		catch (IllegalArgumentException e) {} // eat it; pye has some non-Gaussian component -- that's OK.
 
+if (riso.belief_nets.Global.debug > 0)
+    System.err.println ("IndexedDistribution_Discrete.compute_pi: pye.components.length: "+pye.components.length);
+
 		if ( pye.components.length == 1 ) return pye.components[0];
 		else return pye;
 	}
 
-	int count_nonzero( Distribution[] pi_messages, int[] ii,  int m )
+	void assign_components (Distribution[] pi_messages, int[] ii, int m, IndexedDistribution py, Vector components) throws Exception
 	{
-		if ( m == pi_messages.length )
-		{
-			for ( int i = 0; i < pi_messages.length; i++ )
-			{
-				Discrete pimsg = (Discrete) pi_messages[i];
-				if ( pimsg.probabilities[ ii[i] ] == 0 )
-					return 0;
-			}
-			return 1;
-		}
-		else
-		{
-			int sum = 0;
-			Discrete pimsg = (Discrete) pi_messages[m];
-			for ( ii[m] = 0; ii[m] < pimsg.probabilities.length; ii[m]++ )
-				sum += count_nonzero( pi_messages, ii, m+1 );
-			return sum;
-		}
-	}
-
-	void assign_components( Distribution[] pi_messages, int[] ii, int m, IndexedDistribution py, Mixture pye, int[] nmix, int[] npy ) throws Exception
-	{
-System.err.println( "m: "+m );
-		if ( m == pi_messages.length )
+		if ( m == py.indexes.length )
 		{
 			double alpha = 1;
-			for ( int i = 0; i < pi_messages.length; i++ )
+			for ( int i = 0; i < py.indexes.length; i++ )
 			{
-				Discrete pimsg = (Discrete) pi_messages[i];
+				Discrete pimsg = (Discrete) pi_messages[ py.indexes[i] ];
 				alpha *= pimsg.probabilities[ ii[i] ];
 			}
-System.err.print( "ii: " ); for (int j=0;j<ii.length;j++) System.err.print(ii[j]+" "); System.err.println("");
-System.err.println("alpha: "+alpha);
-			if ( alpha == 0 ) { ++npy[0]; return; }
 
-System.err.println( "nmix: "+nmix[0]+"  npy: "+npy[0] );
-			pye.components[nmix[0]] = (Distribution) py.components[npy[0]].clone();
-			pye.mix_proportions[nmix[0]] = alpha;
-			++nmix[0];
-			++npy[0];
+if (riso.belief_nets.Global.debug > 0)
+{
+    System.err.print ("IndexedDistribution_Discrete.assign_components: ii: ");
+    for (int i = 0; i < ii.length; i++)
+        System.err.print (ii[i]+" "); System.err.println("; alpha: "+alpha);
+}
+
+			if ( alpha == 0 ) return;
+
+            int i_linear = convert_to_linear_index (ii, pi_messages);
+if (riso.belief_nets.Global.debug > 0)
+    System.err.println ("IndexedDistribution_Discrete.assign_components: i_linear: "+i_linear);
+            Object d = py.components [i_linear];
+
+            if (d instanceof Distribution)
+            {
+                d = ((Distribution)d).clone();
+            }
+            else
+            {
+                // d must be a conditional distribution. 
+                // Compute pi for this distribution, using pi messages other than
+                // the ones associated with index parents.
+
+                Distribution[] nonindex_pi_messages = new Distribution [py.non_indexes.length];
+
+                for ( int i = 0; i < py.non_indexes.length; i++ )
+                    nonindex_pi_messages[i] = pi_messages[ py.non_indexes[i] ];
+
+                ConditionalDistribution cpd = (ConditionalDistribution) d;
+                PiHelper helper = PiHelperLoader.load_pi_helper( cpd, nonindex_pi_messages );
+                d = helper.compute_pi (cpd, nonindex_pi_messages);
+            }
+
+if (riso.belief_nets.Global.debug > 0)
+    System.err.println ("IndexedDistribution_Discrete.assign_components: make component with alpha: "+alpha+", description: "+((Distribution)d).format_string("\t"));
+
+            component_pair cp = new component_pair();
+            cp.upd = (Distribution) d;
+            cp.alpha = alpha;
+            components.addElement (cp);
 		}
 		else
 		{
-			Discrete pimsg = (Discrete) pi_messages[m];
+			Discrete pimsg = (Discrete) pi_messages[ py.indexes[m] ];
 			for ( ii[m] = 0; ii[m] < pimsg.probabilities.length; ii[m]++ )
-				assign_components( pi_messages, ii, m+1, py, pye, nmix, npy );
+				assign_components (pi_messages, ii, m+1, py, components);
 		}
 	}
+
+    /** Flatten tuple into a single index: compute addressing polynomial.
+      * Assume row major ordering. Determine dimensions from <tt>pi_messages</tt>.
+      * For now, assume all pi messages are 1-dimensional; should relax that later. !!!
+      */
+    int convert_to_linear_index (int[] ii, Distribution[] pi_messages)
+    {
+        int i_linear = 0;
+
+        for (int i = 0; i < ii.length; i++)
+        {
+            Discrete d = (Discrete) pi_messages[i];
+if (d.dimensions.length > 1) throw new IllegalArgumentException ("IndexedDistribution_Discrete.convert_to_linear_index: pi_messages["+i+"].dimensions.length == "+d.dimensions.length+" > 1; oops.");   // !!!
+            i_linear = i_linear * d.probabilities.length + ii[i];
+        }
+
+        return i_linear;
+    }
 }

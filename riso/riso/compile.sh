ba35ded6b66981daff87eca147d99fc18b8eefeb
robert-dodier <robert.dodier@gmail.com>
2004-04-30 16:06:08 +0000

    reworked: use JavaDeps to determine dependencies and generate makefile; adapted from command line stuff, not tested (although JavaDeps did work on command line)

--- riso/riso/compile.sh
+++ riso/riso/compile.sh
@@ -16,29 +16,21 @@ echo CLASSPATH: $CLASSPATH
 echo JAVAC: $JAVAC
 echo RMIC: $RMIC
 
-$JAVAC belief_nets/Global.java
+pushd ..
+STUFFTOBUILD="riso/{addons,approximation,apps,belief_nets,distributions,general,numerical,regression,remote_data,render}"
 
-pushd remote_data; $JAVAC *.java; popd
-pushd general; $JAVAC *.java; popd
-pushd numerical; $JAVAC *.java; popd
+java smr.JavaDeps.JavaDeps -v -o riso/tmp-riso.deps -d $CLASSDIR `find $STUFFTOBUILD -name \*.java`
 
-pushd distributions
-$JAVAC Abstract* Distribution.java ConditionalDistribution.java MixGaussians.java Gaussian.java Mixture.java LocationScaleDensity.java Translatable.java SupportNotWellDefinedException.java SplineDensity.java
+cat << EOF > riso/tmp-riso.makefile
+JAVACOMPILE=$JAVAC -d $CLASSDIR
+
+default: all
+include tmp-riso.deps
+all: $(CLASSES)
+EOF
+
+make -f riso/tmp-riso.makefile
 popd
-
-pushd approximation; $JAVAC *.java; popd
-pushd regression; $JAVAC *.java; popd
-
-$JAVAC `find belief_nets/ -name \*.java` `find distributions/  -name \*.java`
-
-pushd apps; $JAVAC *.java; popd
-
-pushd addons/dbn; $JAVAC *.java; popd
-
-pushd render; $JAVAC *.java; popd
-
-# FOR NOW, test IS NOT PACKAGED WITH THE OTHER STUFF. !!!
-# pushd test; $JAVAC *.java; popd
 
 $RMIC riso.belief_nets.BeliefNetwork
 $RMIC riso.belief_nets.TemporalBeliefNetwork

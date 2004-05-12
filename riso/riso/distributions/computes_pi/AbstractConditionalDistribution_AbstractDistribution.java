4b2ec5cd29fc5e1d3a5af53ada066fbbcdd49f14
robert-dodier <robert.dodier@gmail.com>
2004-05-12 16:13:17 +0000

    construct description_array in static block; description() method in AbstractPiHelper returns it

--- riso/riso/distributions/computes_pi/AbstractConditionalDistribution_AbstractDistribution.java
+++ riso/riso/distributions/computes_pi/AbstractConditionalDistribution_AbstractDistribution.java
@@ -26,18 +26,18 @@ import riso.general.*;
 
 /** @see PiHelper
  */
-public class AbstractConditionalDistribution_AbstractDistribution implements PiHelper
+public class AbstractConditionalDistribution_AbstractDistribution extends AbstractPiHelper
 {
 	/** Returns a description of the sequences of distributions accepted
 	  * by this helper -- namely one <tt>AbstractConditionalDistribution</tt>
 	  * followed by any number of <tt>AbstractDistribution</tt>.
 	  */
-	public static SeqTriple[] description()
+	static
 	{
 		SeqTriple[] s = new SeqTriple[2];
 		s[0] = new SeqTriple( "riso.distributions.AbstractConditionalDistribution", 1 );
 		s[1] = new SeqTriple( "riso.distributions.AbstractDistribution", -1 );
-		return s;
+		description_array = s;
 	}
 
 	public Distribution compute_pi( ConditionalDistribution pxu, Distribution[] pi_messages ) throws Exception

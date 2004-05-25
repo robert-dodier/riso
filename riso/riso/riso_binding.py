9e279d7c856d5462e9e7a097a4877947a86f1c2c
robert-dodier <robert.dodier@gmail.com>
2004-05-25 02:40:56 +0000

    improvements: got setattr to work so bn.x0 = 0 calls assign_evidence; moved some of the importing stuff into parse_network for easy reuse; __getattr__ returns a function object if attr is a function (that way caller can supply arguments)

--- riso/riso/riso_binding.py
+++ riso/riso/riso_binding.py
@@ -2,24 +2,42 @@ class py_variable:
     def __str__ (self):
         return str (self.java_variable)
     def __getattr__ (self, name):
-        return getattr (self.java_variable, name) ()
+        # COULD LOOK FOR name == 'name' OR 'fullname' AND RETURN THAT
+        return getattr (self.java_variable, name)
 
 class py_bn:
     def __str__ (self):
         return str (self.java_bn)
     def __getattr__ (self, name):
-        return getattr (self.java_bn, name) ()
+        # COULD LOOK FOR name == 'name' OR 'fullname' AND RETURN THAT
+        return getattr (self.java_bn, name)
+    def __setattr__ (self, name, value):
+        try:
+            a = self.__dict__[name]
+            print name+' is in self.__dict__'
+            try:
+                self.java_bn.assign_evidence (a.java_variable, value)
+                print name+' has java_variable attribute'
+            except KeyError:
+                print name+' doesnt have java_variable'
+                a = value
+        except KeyError:
+            print name+' not in self.__dict__'
+            self.__dict__[name] = value
+
+def parse_network (s):
+    b = py_bn ()
+    b.java_bn = c.parse_network (s)
+    l = b.java_bn.get_variables ()
+    for i in range (len (l)):
+        v = py_variable ()
+        v.java_variable = l [i]
+        s = v.java_variable.get_name ()
+        setattr (b, s, v)
+    return b
 
 import riso.belief_nets.BeliefNetworkContext
-c = riso.belief_nets.BeliefNetworkContext('mycontext')
-f = open('/home/robert/belief-nets/random-polytree/random8.riso')
-s = f.read()
-java_bn = c.parse_network(s)
-b = py_bn()
-b.java_bn = java_bn
-l = b.java_bn.get_variables ()
-for i in range(len(l)):
-    v = py_variable ()
-    v.java_variable = l [i]
-    s = v.java_variable.get_name ()
-    setattr (b, s, v)
+c = riso.belief_nets.BeliefNetworkContext ('mycontext')
+f = open ('/home/robert/belief-nets/random-polytree/random8.riso')
+s = f.read ()
+b = parse_network (s)

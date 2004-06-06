c1ecf1ece3b586466331bc2518f4e7c61c4ac6d8
robert-dodier <robert.dodier@gmail.com>
2004-06-06 01:11:56 +0000

    moved assign_bn_attributes code into py_bn.__init__ also, if a variable is assigned None, then execute clear_posterior

--- riso/riso/riso_binding.py
+++ riso/riso/riso_binding.py
@@ -51,11 +51,19 @@ class py_variable:
             return getattr (self.java_variable, name)
 
 class py_bn:
+
     def __init__ (self, java_bn):
         self.java_bn = java_bn
         self.name = java_bn.get_name ()
+        l = java_bn.get_variables ()
+        for i in range (len (l)):
+            v = py_variable (l [i], self)
+            s = v.java_variable.get_name ()
+            setattr (self, s, v)
+
     def __str__ (self):
         return self.java_bn.format_string ('')
+
     def __getattr__ (self, name):
         if name == 'nodes':
             # Attribute nodes doesn't exist yet, so create it.
@@ -68,12 +76,16 @@ class py_bn:
             return self.nodes
         else:
             return getattr (self.java_bn, name)
+
     def __setattr__ (self, name, value):
         try:
             a = self.__dict__[name]
             print name+' is in self.__dict__'
             try:
-                self.java_bn.assign_evidence (a.java_variable, value)
+                if value == None:
+                    self.java_bn.clear_posterior (a.java_variable)
+                else:
+                    self.java_bn.assign_evidence (a.java_variable, value)
                 print name+' has java_variable attribute'
             except KeyError:
                 print name+' doesnt have java_variable'
@@ -84,19 +96,8 @@ class py_bn:
 
 def parse_network (s, c):
     java_bn = c.parse_network (s)
-    b = assign_bn_attributes (java_bn)
-    return b
+    return py_bn (java_bn)
     
-def assign_bn_attributes (java_bn):
-    # HMM, MAYBE THIS WANTS TO MOVE INTO THE py_bn CONSTRUCTOR !!!
-    b = py_bn (java_bn)
-    l = b.java_bn.get_variables ()
-    for i in range (len (l)):
-        v = py_variable (l [i], b)
-        s = v.java_variable.get_name ()
-        setattr (b, s, v)
-    return b
-
 def import_bn (s):
     '''s is a belief network description string'''
     bn = parse_network (s, bn_context)
@@ -115,5 +116,4 @@ def import_bn_file (bn_filename):
 def import_bn_remote (bn_name):
     import java.rmi.Naming
     remote_bn = java.rmi.Naming.lookup ('rmi://'+bn_name)
-    bn = assign_bn_attributes (remote_bn)
-    import_bn_ref (bn)
+    import_bn_ref (py_bn (remote_bn))
